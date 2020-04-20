e6949b6a91204b4ba3c477655891b54636a4e167
Zack Stickles <zstickles@gmail.com>
2020-04-20 15:40:28 -0700

    chore: added sass for styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from './Header';
-import './Layout.css';
+import './Layout.scss';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
