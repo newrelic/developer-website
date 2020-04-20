f2b0b67cff0f2b24b9ac8775e40444719bfa9ae4
Zack Stickles <zstickles@gmail.com>
2020-04-20 16:18:26 -0700

    chore: added linting configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,21 @@ module.exports = {
     es6: true,
   },
   // https://eslint.org/docs/rules
-  extends: "eslint:recommended",
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.jsx'],
+      rules: {
+        'no-undef': 'off',
+        'no-unused-vars': 'off',
+      },
+    },
+  ],
 };
