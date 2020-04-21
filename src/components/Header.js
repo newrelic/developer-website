da0d59a074fb7411ab5f16991c0a1daba7178995
Zack Stickles <zstickles@gmail.com>
2020-04-21 15:07:26 -0700

    chore: container to use throughout site

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,69 +2,78 @@ import { Link } from 'gatsby';
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Container from './Container';
 import './Header.scss';
 
 const Header = ({ links }) => (
   <header className="Header">
-    <nav className="Header-nav Header-nav--nr">
-      <ul>
-        <li>
-          <a href="//newrelic.com" target="_blank" className="Header-nav-link">
-            New Relic
-          </a>
-        </li>
-        <li>
-          <Link to="/" className="Header-nav-link">
-            Developers
-          </Link>
-        </li>
-        <li>
-          <a
-            href="//opensource.newrelic.com"
-            target="_blank"
-            className="Header-nav-link"
-          >
-            Open Source
-          </a>
-        </li>
-        <li>
-          <a
-            href="//docs.newrelic.com"
-            target="_blank"
-            className="Header-nav-link"
-          >
-            Docs
-          </a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <a
-            href="//githib.com/newrelic"
-            target="_blank"
-            className="Header-nav-link"
-          >
-            GitHub
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <Container>
+      <>
+        <nav className="Header-nav Header-nav--nr">
+          <ul>
+            <li>
+              <a
+                href="//newrelic.com"
+                target="_blank"
+                className="Header-nav-link"
+              >
+                New Relic
+              </a>
+            </li>
+            <li>
+              <Link to="/" className="Header-nav-link">
+                Developers
+              </Link>
+            </li>
+            <li>
+              <a
+                href="//opensource.newrelic.com"
+                target="_blank"
+                className="Header-nav-link"
+              >
+                Open Source
+              </a>
+            </li>
+            <li>
+              <a
+                href="//docs.newrelic.com"
+                target="_blank"
+                className="Header-nav-link"
+              >
+                Docs
+              </a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a
+                href="//githib.com/newrelic"
+                target="_blank"
+                className="Header-nav-link"
+              >
+                GitHub
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-    <h1 className="Header-title">
-      <Link to="/">{'</>'} New Relic Developers</Link>
-    </h1>
+        <h1 className="Header-title">
+          <Link to="/">{'</>'} New Relic Developers</Link>
+        </h1>
 
-    <nav className="Header-nav Header-nav--main">
-      <ul>
-        {links.map((link, i) => (
-          <li key={i}>
-            <Link to={link.url} className="Header-nav-link">
-              {link.displayName}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+        <nav className="Header-nav Header-nav--main">
+          <ul>
+            {links.map((link, i) => (
+              <li key={i}>
+                <Link to={link.url} className="Header-nav-link">
+                  {link.displayName}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </>
+    </Container>
   </header>
 );
 
