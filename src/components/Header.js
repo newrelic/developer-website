fa54ae2cfd1d39167d1f400063b6c23844c0770b
Zack Stickles <zstickles@gmail.com>
2020-04-21 14:14:30 -0700

    chore: basic stricture and style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,38 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => (
-  <header>
-    <h1>
-      <Link to="/">Developer Site</Link>
+import './Header.scss';
+
+const Header = ({ links }) => (
+  <header className="Header">
+    <nav className="Header-navigation--nr"></nav>
+
+    <h1 className="Header-title">
+      <Link to="/">{'</>'} New Relic Developer</Link>
     </h1>
+
+    <nav className="Header-navigation--main"></nav>
   </header>
 );
+
+Header.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      displayName: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+Header.defaultProps = {
+  links: [
+    { displayName: 'Collect Data', url: '' },
+    { displayName: 'Explore Data', url: '' },
+    { displayName: 'Build Apps', url: '' },
+    { displayName: 'Automate New Relic', url: '' },
+    { displayName: 'Reference Docs', url: '' },
+  ],
+};
 
 export default Header;
