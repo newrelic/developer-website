0e29403155ca4710b72daf279045932d44d0fb83
Zack Stickles <zstickles@gmail.com>
2020-04-21 11:43:01 -0700

    chore: lower-cased file names and imports

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-import Layout from '../components/Layout';
-import SEO from '../components/Seo';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const SecondPage = () => (
   <Layout>
