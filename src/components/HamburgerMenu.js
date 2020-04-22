ecc9e1f5d68d09989fc202b6c51960297715a622
Zack Stickles <zstickles@gmail.com>
2020-04-22 14:51:43 -0700

    feat: created hamburger menu component

---
+++ src/components/HamburgerMenu.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import './HamburgerMenu.scss';
+
+const HamburgerMenu = ({ onClick }) => {
+  const [open, updateOpen] = useState(false);
+
+  return (
+    <button
+      aria-expanded={open}
+      aria-label="Mobile Menu"
+      className={cx('HamburgerMenu', { HamburgerMenu__open: open })}
+      onClick={() => {
+        updateOpen(!open);
+        onClick();
+      }}
+    >
+      <div />
+      <div />
+      <div />
+    </button>
+  );
+};
+
+HamburgerMenu.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default HamburgerMenu;
