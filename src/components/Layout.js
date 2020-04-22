f9e3066fe1e829d802d48a22f9f48a82ad8e9e31
Zack Stickles <zstickles@gmail.com>
2020-04-22 12:08:16 -0700

    fix: resolving file name issues

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Header from './Header';
-import './Layout.scss';
+import './styles.scss';
 
 const Layout = ({ children }) => (
   <>
