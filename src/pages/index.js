52e8098285fdd0d3cfc62110e3605e7eea6822ee
lbaker <lizbaker.pdx@gmail.com>
2020-04-22 16:59:25 -0700

    chore: Swap h2 toggle logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Jumbotron />
-    <h2 className="indexPage-h2">
-      {window.screenX >= 500
-        ? 'Learn about observability with one of our guides'
-        : 'Learn how with one of our guides'}
+    <h2 className="indexPage-h2 desktop">
+      Learn about observability with one of our guides
     </h2>
+    <h2 className="indexPage-h2 mobile">Learn how with one of our guides</h2>
     <div className="indexPage-grid">
       <div className="indexPage-grid-guide">Collect Data</div>
       <div className="indexPage-grid-guide">Explore Data</div>
