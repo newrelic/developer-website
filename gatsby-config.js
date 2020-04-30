22368ae9a2e5861b80e93658cc6fa3a712d026c4
lbaker <lizbaker.pdx@gmail.com>
2020-04-30 16:32:25 -0700

    chore: Remove gatsby cms.js from config (for now)

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,9 +38,6 @@ module.exports = {
     `gatsby-transformer-remark`,
     {
       resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
     },
   ],
 };
