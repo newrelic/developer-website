26741acad46b1a3d06d7a6bcd36cd43098202f79
lbaker <lizbaker.pdx@gmail.com>
2020-04-30 11:51:31 -0700

    chore: Install cms plugin and create branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,5 +36,11 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ],
 };
