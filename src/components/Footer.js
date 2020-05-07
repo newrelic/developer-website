aa91b8b29318f447c6c1d6416afdd49a9cdd9186
Zack Stickles <zstickles@gmail.com>
2020-05-07 15:37:49 -0700

    feat: desktop footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
 import Container from './Container';
+import { link } from '../types';
 
 import './Footer.scss';
 
-const Footer = () => (
-  <div className="Footer">
-    <Container>Footer goes here, wow!</Container>
-  </div>
+const Footer = ({ pages }) => (
+  <footer className="Footer">
+    <Container>
+      <div className="Footer-left">
+        <Link to="/" className="logo Footer-logo" />
+        <div className="Footer-copyright">
+          Copyright &copy; 2020 New Relic Inc.
+        </div>
+      </div>
+
+      <nav role="navigation" aria-label="Footer" className="Footer-nav">
+        <ul>
+          {pages.map((page, i) => (
+            <li key={i}>
+              <Link to={page.url}>{page.displayName}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </Container>
+  </footer>
 );
+
+Footer.propTypes = {
+  pages: PropTypes.arrayOf(link),
+};
+
+Footer.defaultProps = {
+  pages: [
+    { displayName: 'Collect Data', url: '' },
+    { displayName: 'Explore Data', url: 'explore-data' },
+    { displayName: 'Build Apps', url: '' },
+    { displayName: 'Automate Workflows', url: '' },
+    { displayName: 'Developer Docs', url: '' },
+  ],
+};
 
 export default Footer;
