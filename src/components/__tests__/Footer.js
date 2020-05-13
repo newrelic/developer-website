7c3bf35f7fafed84679298855da1f7af8d005346
Zack Stickles <zstickles@gmail.com>
2020-05-13 15:11:15 -0700

    feat: example component snapshot test

---
+++ src/components/__tests__/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Footer from '../Footer';
+
+describe('Footer', () => {
+  it('renders correctly', () => {
+    const pages = [
+      { displayName: 'One', url: '/one' },
+      { displayName: 'Two', url: '/two' },
+    ];
+    const tree = renderer.create(<Footer pages={[]} />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
