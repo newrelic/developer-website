787016f386cc95d204c00334cb884f6585176a59
Zack Stickles <zstickles@gmail.com>
2020-05-14 11:42:27 -0700

    chore: created tests that pass

---
+++ src/utils/__tests__/nav-from-edges.js
@@ -0,0 +1,48 @@
+import navFromEdges from '../nav-from-edges';
+
+// hepler function to create an edge object
+const createEdge = (frontmatter) => ({
+  node: {
+    frontmatter,
+  },
+});
+
+describe('[util] navFromEdges', () => {
+  it('creates an array of nested links', () => {
+    const edges = [
+      createEdge({ title: 'One', path: '/one/index' }),
+      createEdge({ title: 'OneTwo', path: '/one/two' }),
+      createEdge({ title: 'ThreeFourFive', path: '/three/four/five' }),
+    ];
+
+    const actual = navFromEdges(edges);
+    const expected = [
+      {
+        displayName: 'One',
+        url: '/one/index',
+        children: [
+          {
+            displayName: 'OneTwo',
+            url: '/one/two',
+          },
+        ],
+      },
+      {
+        displayName: 'Three',
+        children: [
+          {
+            displayName: 'Four',
+            children: [
+              {
+                displayName: 'ThreeFourFive',
+                url: '/three/four/five',
+              },
+            ],
+          },
+        ],
+      },
+    ];
+
+    expect(actual).toEqual(expected);
+  });
+});
