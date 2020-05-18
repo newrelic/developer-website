447e68c22fa26822c030d78f2b84e4388a0293bf
Tim Glaser <tglaser@newrelic.com>
2020-05-18 14:39:06 -0700

    feat: populate side nav from json file

--- src/pages/reference.js
+++ src/pages/reference.js
@@ -1,32 +1,15 @@
 import React, { useState } from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-
 import Container from '../components/Container';
 import Layout from '../components/Layout';
 import Sidebar from '../components/Sidebar';
-import navFromEdges from '../utils/nav-from-edges';
+
+import pages from '../data/sidenav.json';
 
 // TODO: move this js file to same directory and update import
 import '../templates/Reference.scss';
 
 const Reference = () => {
   const [isOpen, setIsOpen] = useState(false);
-
-  const data = useStaticQuery(graphql`
-    query {
-      allMarkdownRemark(limit: 1000) {
-        edges {
-          node {
-            frontmatter {
-              path
-              title
-            }
-          }
-        }
-      }
-    }
-  `);
-  const pages = navFromEdges(data.allMarkdownRemark.edges);
 
   return (
     <Layout>
