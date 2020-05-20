5af843640e0bed9c66c75a5d311866ab81af715d
lbaker <lizbaker.pdx@gmail.com>
2020-05-20 10:15:24 -0700

    chore: Add basic styling to code snippet

--- src/components/CodeSnippet.js
+++ src/components/CodeSnippet.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './CodeSnippet.scss';
 
-const CodeSnippet = ({ children }) => <div>{children}</div>;
+const CodeSnippet = ({ children }) => (
+  <>
+    <div className="CodeSnippet">{children}</div>
+    <div className="CodeSnippet-copyBar" />
+  </>
+);
 
 CodeSnippet.propTypes = {
   children: PropTypes.node,
