141bf432cf40bd04e78af1223f19219c65176259
lbaker <lizbaker.pdx@gmail.com>
2020-05-21 16:38:30 -0700

    chore: Add styling and copy button functionality

--- src/components/CodeSnippet.js
+++ src/components/CodeSnippet.js
@@ -2,22 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import github from 'prism-react-renderer/themes/github';
+import styles from './CodeSnippet.module.scss';
 
-const CodeSnippet = ({ children, copy, className }) => {
+const copyCode = (code) => {
+  const textArea = document.createElement('textarea');
+  textArea.value = code;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+};
+
+const CodeSnippet = ({ children, copy, className, lineNumbers }) => {
   const language = className.replace('language-', '');
+
   return (
-    <div className="CodeSnippet">
-      <div className="CodeSnippet-code">
+    <div>
+      <div className={styles.CodeSnippet}>
         <Highlight
           {...defaultProps}
           theme={github}
           code={children}
           language={language}
         >
-          {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre className={className} style={{ ...style, padding: '20px' }}>
+          {({ style, tokens, getLineProps, getTokenProps }) => (
+            <pre style={{ ...style, padding: '20px' }}>
               {tokens.map((line, i) => (
                 <div key={i} {...getLineProps({ line, key: i })}>
+                  {lineNumbers !== 'false' && i < tokens.length - 1 && (
+                    <span className={styles.lineNumber}>{i + 1}</span>
+                  )}
                   {line.map((token, key) => (
                     <span key={key} {...getTokenProps({ token, key })} />
                   ))}
@@ -27,7 +41,13 @@ const CodeSnippet = ({ children, copy, className }) => {
           )}
         </Highlight>
       </div>
-      {copy !== 'false' && <div className="CodeSnippet-copy">COPY ME</div>}
+      {copy !== 'false' && (
+        <div className={styles.CodeSnippetCopyBar}>
+          <button type="button" onClick={() => copyCode(children)}>
+            Copy output
+          </button>
+        </div>
+      )}
     </div>
   );
 };
@@ -36,10 +56,12 @@ CodeSnippet.propTypes = {
   children: PropTypes.node.isRequired,
   copy: PropTypes.string, // TODO: limit options
   className: PropTypes.string,
+  lineNumbers: PropTypes.string,
 };
 
 CodeSnippet.defaultProps = {
   copy: 'true',
+  lineNumbers: 'true',
   className: 'language-javascript',
 };
 
