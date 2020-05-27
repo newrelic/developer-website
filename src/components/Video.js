581604edfdceed2173d1fb48f6e765c915d80d45
lbaker <lizbaker.pdx@gmail.com>
2020-05-27 13:31:46 -0700

    chore: Add wistia support for embedded video

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,21 +3,30 @@ import PropTypes from 'prop-types';
 
 import styles from './Video.module.scss';
 
-const Video = ({ youtubeId, title }) => (
-  <div className={styles.Video}>
-    <iframe
-      src={`//www.youtube.com/embed/${youtubeId}?modestbranding=1`}
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      title={title}
-      frameBorder="0"
-      allowFullScreen
-    />
-  </div>
-);
+const Video = ({ wistiaId, youtubeId, title }) => {
+  const embedYoutube = youtubeId
+    ? `//www.youtube.com/embed/${youtubeId}?modestbranding=1`
+    : null;
+  const embedWistia = wistiaId
+    ? `//fast.wistia.net/embed/iframe/${wistiaId}`
+    : null;
+  return (
+    <div className={styles.Video}>
+      <iframe
+        src={embedYoutube || embedWistia}
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        title={title}
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
+  );
+};
 
 Video.propTypes = {
   // NOTE: we should expand this allow for other video sources in the future
-  youtubeId: PropTypes.string.isRequired,
+  youtubeId: PropTypes.string,
+  wistiaId: PropTypes.string,
   title: PropTypes.string,
 };
 
