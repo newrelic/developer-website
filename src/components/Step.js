aec12e7464c9d0bb2842494e446b097ce7dffa74
lbaker <lizbaker.pdx@gmail.com>
2020-05-29 14:08:33 -0700

    chore: Refactor step component

--- src/components/Step.js
+++ src/components/Step.js
@@ -2,19 +2,26 @@ import React from 'react';
 import styles from './Step.module.scss';
 import Proptypes from 'prop-types';
 
-const Step = ({ children, number, total, title }) => (
-  <div className={styles.wrapper}>
-    <p className={styles.stepNumber}>{`Step ${number} of ${total}`}</p>
-    {title && <p className={styles.title}>{title}</p>}
-    <div className={styles.container}>{children}</div>
-  </div>
-);
+const Step = ({ children, number, total }) => {
+  const codeSnippet = children.find((child) => child?.props?.mdxType === 'pre');
+  const childrenWithoutCodeSnippet = children.filter(
+    (child) => child !== codeSnippet
+  );
+  return (
+    <div className={styles.wrapper}>
+      <p className={styles.stepNumber}>{`Step ${number} of ${total}`}</p>
+      <div className={styles.container}>
+        <div className={styles.stepDetails}>{childrenWithoutCodeSnippet}</div>
+        {codeSnippet}
+      </div>
+    </div>
+  );
+};
 
 Step.propTypes = {
   children: Proptypes.node.isRequired,
   number: Proptypes.number.isRequired,
   total: Proptypes.number.isRequired,
-  title: Proptypes.string,
 };
 
 export default Step;
