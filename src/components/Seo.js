cbaa161124883106f0444ef6f4e77bd866665b81
Zack Stickles <zstickles@gmail.com>
2020-05-29 13:42:58 -0700

    chore: added best-practices meta

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -21,17 +21,18 @@ function SEO({ description, lang, meta, title }) {
   const metaDescription = description || site.siteMetadata.description;
 
   const siteWideMeta = [
-    { name: `description`, content: metaDescription },
-    { property: `og:title`, content: title },
-    { property: `og:description`, content: metaDescription },
-    { property: `og:type`, content: `website` },
+    { name: 'description', content: metaDescription },
+    { 'http-equiv': 'Content-Type', content: 'text/html', charset: 'utf-8' },
   ];
 
   const social = [
-    { name: `twitter:card`, content: `summary` },
-    { name: `twitter:creator`, content: site.siteMetadata.author },
-    { name: `twitter:title`, content: title },
-    { name: `twitter:description`, content: metaDescription },
+    { property: 'og:title', content: title },
+    { property: 'og:description', content: metaDescription },
+    { property: 'og:type', content: 'website' },
+    { name: 'twitter:card', content: 'summary' },
+    { name: 'twitter:creator', content: site.siteMetadata.author },
+    { name: 'twitter:title', content: title },
+    { name: 'twitter:description', content: metaDescription },
   ];
 
   return (
