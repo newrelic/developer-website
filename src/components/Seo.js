63dff108f057372e23c25de2ff2b4e35b321c102
Zack Stickles <zstickles@gmail.com>
2020-05-29 13:31:31 -0700

    chore: formatting site metadata

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
@@ -27,47 +20,26 @@ function SEO({ description, lang, meta, title }) {
 
   const metaDescription = description || site.siteMetadata.description;
 
+  const siteWideMeta = [
+    { name: `description`, content: metaDescription },
+    { property: `og:title`, content: title },
+    { property: `og:description`, content: metaDescription },
+    { property: `og:type`, content: `website` },
+  ];
+
+  const social = [
+    { name: `twitter:card`, content: `summary` },
+    { name: `twitter:creator`, content: site.siteMetadata.author },
+    { name: `twitter:title`, content: title },
+    { name: `twitter:description`, content: metaDescription },
+  ];
+
   return (
     <Helmet
-      htmlAttributes={{
-        lang,
-      }}
+      htmlAttributes={{ lang }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
+      meta={[...siteWideMeta, ...social, ...meta]}
     />
   );
 }
