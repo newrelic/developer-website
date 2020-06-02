b2ef50316258978a6ead0c2c0d96bdbe29b442af
Jerel Miller <jmiller@newrelic.com>
2020-06-02 15:52:20 -0700

    feat: add styles for each component example

--- src/components/ComponentExample.js
+++ src/components/ComponentExample.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import formatCode from '../utils/formatCode';
 import github from 'prism-react-renderer/themes/github';
 import { LiveEditor, LiveError, LiveProvider, LivePreview } from 'react-live';
+import styles from './ComponentExample.module.scss';
 
 const TRAILING_SEMI = /;\s*$/;
 
 const ComponentExample = ({ className, example }) => (
-  <div className={className}>
-    <h3>{example.label}</h3>
+  <div className={cx(styles.container, className)}>
+    <h3 className={styles.title}>{example.label}</h3>
     <LiveProvider
       scope={window.__NR1_SDK__.default}
       code={formatCode(example.sourceCode).replace(TRAILING_SEMI, '')}
       theme={github}
     >
-      <LivePreview />
+      <LivePreview className={styles.preview} />
       <LiveEditor />
-      <LiveError />
+      <LiveError className={styles.error} />
     </LiveProvider>
   </div>
 );
