a11b911c00f7939b32ffcbda74ee17067e9ee9a2
lbaker <lizbaker.pdx@gmail.com>
2020-06-05 10:07:33 -0700

    chore: Tweak step component for text only steps

--- src/components/Step.js
+++ src/components/Step.js
@@ -3,16 +3,26 @@ import styles from './Step.module.scss';
 import Proptypes from 'prop-types';
 
 const Step = ({ children, number, total }) => {
-  const codeSnippet = children.find((child) => child?.props?.mdxType === 'pre');
-  const childrenWithoutCodeSnippet = children.filter(
-    (child) => child !== codeSnippet
-  );
+  let codeSnippet = null;
+  let childrenWithoutCodeSnippet = null;
+  if (children.length) {
+    codeSnippet = children.find((child) => child?.props?.mdxType === 'pre');
+    childrenWithoutCodeSnippet = children.filter(
+      (child) => child !== codeSnippet
+    );
+  }
   return (
     <div className={styles.wrapper}>
       <p className={styles.stepNumber}>{`Step ${number} of ${total}`}</p>
       <div className={styles.container}>
-        <div className={styles.stepDetails}>{childrenWithoutCodeSnippet}</div>
-        {codeSnippet}
+        <div
+          className={`${styles.stepDetails} ${
+            codeSnippet && styles.stepDetailsWithCode
+          }`}
+        >
+          {childrenWithoutCodeSnippet || children}
+        </div>
+        {codeSnippet && codeSnippet}
       </div>
     </div>
   );
