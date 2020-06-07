09d8a69418ba84364a8b09b72222714c113493a0
Jerel Miller <jmiller@newrelic.com>
2020-06-07 02:07:59 -0700

    feat: Render union type info for union props

--- src/components/PropTypeInfo.js
+++ src/components/PropTypeInfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FunctionDefinition from './FunctionDefinition';
+import styles from './PropTypeInfo.module.scss';
 
 const PropTypeInfo = ({ type }) => {
   switch (type.raw) {
@@ -10,6 +11,18 @@ const PropTypeInfo = ({ type }) => {
           returnValue={type.meta.returnValue}
           params={type.meta.params}
         />
+      );
+    case 'oneOf':
+      return (
+        <div className={styles.listLike}>
+          <div>{'<One of'}</div>
+          <div className={styles.arg}>
+            {type.meta.constants.map((constant) => (
+              <div key={constant}>{constant},</div>
+            ))}
+          </div>
+          <div>{'>'}</div>
+        </div>
       );
     default:
       return null;
