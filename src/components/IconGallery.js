625b76488b353e89d1bba41b9e295ad6cac5ee72
Zack Stickles <zstickles@gmail.com>
2020-06-11 08:20:49 -0700

    chore: display icon grid

--- src/components/IconGallery.js
+++ src/components/IconGallery.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useMemo } from 'react';
 import styles from './IconGallery.module.scss';
 
-const IconGallery = ({ icons }) => {
+const IconGallery = () => {
+  if (typeof window === 'undefined') global.window = {};
+
+  // Get the Icon component when available
+  const Icon = useMemo(() => window.__NR1_SDK__.default.Icon, [
+    window?.__NR1_SDK__,
+  ]);
+  console.log('foo');
+
+  // If we don't have the component, don't render anything
+  if (!Icon) return null;
+
   return (
     <>
       <h2>Icon Gallery</h2>
       <div className={styles.iconGrid}>
-        {icons.map((icon, index) => (
-          <div key={index}>{icon.slice(-5)}</div>
+        {Object.keys(Icon.TYPE).map((type, index) => (
+          <div key={index}>
+            <Icon className={styles.icon} type={Icon.TYPE[type]} />
+            <span className={styles.iconName}>{type}</span>
+          </div>
         ))}
       </div>
     </>
   );
-};
-
-IconGallery.propTypes = {
-  icons: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default IconGallery;
