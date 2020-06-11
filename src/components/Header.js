703151d1ce1f1f2fa3b4ee09abc847a3f7ddc024
Cayla Hamann <chamann@newrelic.com>
2020-06-11 09:55:55 -0400

    chore: header removed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import cx from 'classnames';
-
-import { link } from '../types';
 import Container from './Container';
 import ExternalLink from './ExternalLink';
 import HamburgerMenu from './HamburgerMenu';
 import styles from './Header.module.scss';
 
-const Header = ({ pages }) => {
+const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -42,49 +39,18 @@ const Header = ({ pages }) => {
           </ul>
         </nav>
 
-        <h1 className={styles.title}>
+        {/* <h1 className={styles.title}>
           <Link to="/" className={cx(styles.logo, styles.titleLogo)} />
-        </h1>
+        </h1> */}
 
         <HamburgerMenu
           className={styles.hamburgerMenu}
           toggle={() => setIsOpen(!isOpen)}
           isOpen={isOpen}
         />
-
-        <nav role="navigation" aria-label="Main" className={styles.navMain}>
-          <h3 className={styles.hideOnDesktop}>Developers</h3>
-          <ul>
-            {pages.map((page, i) => (
-              <li key={i}>
-                <Link to={page.url}>{page.displayName}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-
-        <nav className={styles.navTools}>
-          <h3 className={styles.hideOnDesktop}>Tools</h3>
-          <ul>
-            <li>
-              <ExternalLink
-                className={cx(styles.logo, styles.logoGithub)}
-                href="//github.com/newrelic"
-              >
-                <span className={styles.hideOnDesktop}>
-                  Contribute on GitHub
-                </span>
-              </ExternalLink>
-            </li>
-          </ul>
-        </nav>
       </Container>
     </header>
   );
-};
-
-Header.propTypes = {
-  pages: PropTypes.arrayOf(link),
 };
 
 export default Header;
