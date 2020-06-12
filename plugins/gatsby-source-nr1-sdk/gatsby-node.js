f6cc594a99895e961aac44bfb578c19b3e53e456
Jerel Miller <jmiller@newrelic.com>
2020-06-12 00:49:07 -0700

    feat: Add node for button component

--- plugins/gatsby-source-nr1-sdk/gatsby-node.js
+++ plugins/gatsby-source-nr1-sdk/gatsby-node.js
@@ -1,9 +1,27 @@
 const loadSdk = require('./loadSdk');
 
-exports.sourceNodes = async ({ actions }, { release }) => {
-  // const { createNode } = actions;
+exports.sourceNodes = async (
+  { actions, createNodeId, createContentDigest },
+  { release }
+) => {
+  const { createNode } = actions;
 
   const sdk = await loadSdk(release);
+  const { Button } = sdk;
 
-  console.log(sdk);
+  const data = {
+    name: 'Button',
+    description: Button.__docs__.text,
+  };
+
+  createNode({
+    ...data,
+    id: createNodeId('NR1SdkComponent-Button'),
+    parent: null,
+    children: [],
+    internal: {
+      type: 'NR1SdkComponent',
+      contentDigest: createContentDigest(data),
+    },
+  });
 };
