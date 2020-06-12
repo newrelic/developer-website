169bf13dfc6bb419862f6378df1845c95b2a2b53
Zack Stickles <zstickles@gmail.com>
2020-06-12 10:20:57 -0700

    feat: adds the (optional) ability to have other content in intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,14 +4,20 @@ import PropTypes from 'prop-types';
 
 import styles from './Intro.module.scss';
 
-const Intro = ({ children }) => (
-  <SideBySide type="Video" className={styles.container}>
+const Intro = ({ children, type }) => (
+  <SideBySide type={type} className={styles.container}>
     {children}
   </SideBySide>
 );
 
 Intro.propTypes = {
   children: PropTypes.node.isRequired,
+  // Only video or code is supported at the moment
+  type: PropTypes.oneOf('Video', 'pre'),
+};
+
+Intro.defaultProps = {
+  type: 'Video',
 };
 
 export default Intro;
