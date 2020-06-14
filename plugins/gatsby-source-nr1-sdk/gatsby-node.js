43f3f01bf24ef2706d00cc0c0865b557f0dac4d1
Jerel Miller <jmiller@newrelic.com>
2020-06-14 16:11:15 -0700

    refactor: Simplify loops when creating docs

--- plugins/gatsby-source-nr1-sdk/gatsby-node.js
+++ plugins/gatsby-source-nr1-sdk/gatsby-node.js
@@ -7,47 +7,45 @@ exports.sourceNodes = async (
   const { createNode } = actions;
   const sdk = await loadSdk(release);
 
-  const documentedComponents = components
-    .map((name) => ({ name, component: sdk[name] }))
-    .filter(({ component }) => Boolean(component));
+  components
+    .filter((name) => Boolean(sdk[name]))
+    .forEach((name) => {
+      const component = sdk[name];
+      const data = {
+        name,
+        description: component.__docs__.text,
+      };
 
-  const documentedAPIs = apis
-    .map((name) => ({ name, api: sdk[name] }))
-    .filter(({ api }) => Boolean(api));
-
-  documentedComponents.forEach(({ name, component }) => {
-    const data = {
-      name,
-      description: component.__docs__.text,
-    };
-
-    createNode({
-      ...data,
-      id: createNodeId(`NR1SdkComponent-${name}`),
-      parent: null,
-      children: [],
-      internal: {
-        type: 'NR1SdkComponent',
-        contentDigest: createContentDigest(data),
-      },
+      createNode({
+        ...data,
+        id: createNodeId(`SDKComponent-${name}`),
+        parent: null,
+        children: [],
+        internal: {
+          type: 'SDKComponent',
+          contentDigest: createContentDigest(data),
+        },
+      });
     });
-  });
 
-  documentedAPIs.forEach(({ name, api }) => {
-    const data = {
-      name,
-      description: api.__docs__.text,
-    };
+  apis
+    .filter((name) => Boolean(sdk[name]))
+    .forEach((name) => {
+      const api = sdk[name];
+      const data = {
+        name,
+        description: api.__docs__.text,
+      };
 
-    createNode({
-      ...data,
-      id: createNodeId(`NR1SdkAPI-${name}`),
-      parent: null,
-      children: [],
-      internal: {
-        type: 'NR1SdkAPI',
-        contentDigest: createContentDigest(data),
-      },
+      createNode({
+        ...data,
+        id: createNodeId(`SDKApi-${name}`),
+        parent: null,
+        children: [],
+        internal: {
+          type: 'SDKApi',
+          contentDigest: createContentDigest(data),
+        },
+      });
     });
-  });
 };
