8e5dd800d45c430ec8dc4a2a6a203e2243497739
Jerel Miller <jmiller@newrelic.com>
2020-06-15 16:55:54 -0700

    chore: Polish styles on type def reference

--- src/components/TypeDefReference.js
+++ src/components/TypeDefReference.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Children } from 'react';
 import Markdown from 'react-markdown';
 import PropTypes from 'prop-types';
 import styles from './TypeDefReference.module.scss';
 
-const RenderDescription = ({ children }) => (
-  <span className={styles.description}>
-    <p>{` //`}</p> {children}
-  </span>
-);
+const RenderDescription = ({ children, ...props }) => {
+  if (Children.toArray(children).length === 0) {
+    return null;
+  }
+
+  return (
+    <span {...props} className={styles.description}>
+      {` //`} {children}
+    </span>
+  );
+};
 
 const RenderProperty = ({ property }) => {
   const { type, name, description } = property;
 
   return (
     <div key={name} className={styles.propertyContainer}>
-      <span className={styles.propertyName}>{name}</span>
+      <span className={styles.propertyName}>{name}:</span>
       <span className={styles.type}>{type}</span>,
       <Markdown
         source={description}
@@ -30,15 +36,17 @@ const TypeDefReference = ({ typeDef }) => {
   const { properties, name } = typeDef;
 
   return (
-    <div className={styles.container} key={name}>
-      <div className={styles.name}>{name}</div>
-      <div className={styles.block}>
-        <div className={styles.topBracket}>{`{`}</div>
+    <div className={styles.container}>
+      <h3 className={styles.name}>
+        <code>{name}</code>
+      </h3>
+      <code className={styles.typeDef}>
+        <div className={styles.syntax}>{`{`}</div>
         {properties.map((property, i) => (
           <RenderProperty key={i} property={property} />
         ))}
-        <div>{`}`}</div>
-      </div>
+        <div className={styles.syntax}>{'}'}</div>
+      </code>
     </div>
   );
 };
