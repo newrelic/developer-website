b5da148c5f0046a1aed08c403a76a10c8935015b
lbaker <lizbaker.pdx@gmail.com>
2020-06-15 11:39:30 -0700

    chore: Update 404 page styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
+import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/Seo';
+import Section from '../components/Section';
+import styles from './404.module.scss';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Section backgroundBanner className={styles.backgroundBanner}>
+      <div className={styles.messageContainer}>
+        <h1 className={styles.header}>404</h1>
+        <p>
+          This page does not exist. Check the URL and try again, or{' '}
+          <Link to="/">go home</Link>.
+        </p>
+      </div>
+    </Section>
   </Layout>
 );
 
