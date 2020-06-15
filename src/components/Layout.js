2146cd58e7c3d69dbaa9a1aad9fe922df6e648f1
Cayla Hamann <38332422+caylahamann@users.noreply.github.com>
2020-06-15 12:56:19 -0400

    Update src/components/Layout.js
    Co-authored-by: Zack Stickles <zstickles@gmail.com>

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,13 +8,6 @@ import styles from './Layout.module.scss';
 import pages from '../data/sidenav.json';
 import './styles.scss';
 
-// const pages = [
-//   { displayName: 'Collect data', url: '/collect-data' },
-//   { displayName: 'Explore data', url: '/explore-data' },
-//   { displayName: 'Build apps', url: '/build-apps' },
-//   { displayName: 'Automate workflows', url: '/automate-workflows' },
-//   { displayName: 'Developer docs', url: '/docs' },
-// ];
 
 const Layout = ({ children }) => (
   <div className={styles.layout}>
