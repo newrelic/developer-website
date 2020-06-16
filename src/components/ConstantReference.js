83d33f841bf35485b8fdb76221b6d57bbf2ea217
Jerel Miller <jmiller@newrelic.com>
2020-06-15 19:55:53 -0700

    refactor: Extract value to JsonValue component

--- src/components/ConstantReference.js
+++ src/components/ConstantReference.js
@@ -1,67 +1,8 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import CodeDef from './CodeDef';
+import JsonValue from './JsonValue';
 import styles from './ConstantReference.module.scss';
-
-const Value = ({ value }) => {
-  const type = typeof value;
-
-  if (Array.isArray(value)) {
-    return (
-      <>
-        <CodeDef.Bracket>[</CodeDef.Bracket>
-        <CodeDef.Block>
-          {value.map((item, idx) => (
-            <div key={idx}>
-              <Value value={item} />
-              {idx !== value.length - 1 && ','}
-            </div>
-          ))}
-        </CodeDef.Block>
-        <CodeDef.Bracket>]</CodeDef.Bracket>
-      </>
-    );
-  }
-
-  if (value == null) {
-    return <CodeDef.Keyword>{String(value)}</CodeDef.Keyword>;
-  }
-
-  console.log({ value, type });
-
-  switch (type) {
-    case 'object':
-      return (
-        <>
-          <CodeDef.Bracket>{'{ '}</CodeDef.Bracket>
-          <CodeDef.Block>
-            {Object.entries(value).map(([key, value], idx, arr) => (
-              <div key={key}>
-                <CodeDef.Identifier>{key}: </CodeDef.Identifier>
-                <Value value={value} />
-                {idx !== arr.length - 1 && ', '}
-              </div>
-            ))}
-          </CodeDef.Block>
-          <CodeDef.Bracket>{' }'}</CodeDef.Bracket>
-        </>
-      );
-    case 'string':
-      return <CodeDef.String value={value} />;
-    case 'number':
-      return <CodeDef.Number value={value} />;
-    default:
-      return value;
-  }
-};
-
-Value.propTypes = {
-  value: PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.array,
-    PropTypes.string,
-  ]),
-};
 
 const ConstantReference = ({ constant }) => {
   const { name, value } = constant;
@@ -71,7 +12,7 @@ const ConstantReference = ({ constant }) => {
       <h3 className={styles.name}>
         <code>{name}</code>
       </h3>
-      <Value value={value} />
+      <JsonValue value={value} />
     </CodeDef>
   );
 };
