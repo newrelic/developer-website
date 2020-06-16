8ce13353edfeae9abec8239496bfeb8ed9bd2bde
Jerel Miller <jmiller@newrelic.com>
2020-06-16 13:49:18 -0700

    feat: Use Logo component in footer. Add props to change text/bracket color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styles from './Footer.module.scss';
+import Logo from './Logo';
 
 const Footer = () => (
   <footer className={styles.footer}>
     <div className={styles.leftColumn}>
-      <Link to="/" className={`${styles.logo} logo`} />
+      <Link to="/">
+        <Logo
+          className={styles.logo}
+          textColor="#7DA5A8"
+          bracketColor="#7DA5A8"
+        />
+      </Link>
       <div className={styles.copyright}>
         Copyright &copy; 2020 New Relic Inc.
       </div>
