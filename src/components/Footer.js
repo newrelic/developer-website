24cea99ee08825de3d3f68b743b4ea237d4d2f1e
Zack Stickles <zstickles@gmail.com>
2020-06-16 10:10:46 -0700

    feat: added links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
+import cx from 'classnames';
 import styles from './Footer.module.scss';
 
-const Footer = () => (
+const Footer = ({ showEdit }) => (
   <footer className={styles.footer}>
-    <div className={styles.leftColumn}>
-      <Link to="/" className={`${styles.logo} logo`} />
-      <div className={styles.copyright}>
-        Copyright &copy; 2020 New Relic Inc.
+    <div className={cx('container', styles.container)}>
+      <div className={styles.left}>
+        <Link to="/" className={`${styles.logo} logo`} />
+        <div className={styles.copyright}>
+          Copyright &copy; 2020 New Relic Inc.
+        </div>
+      </div>
+
+      <div className={styles.right}>
+        {showEdit && (
+          <Link to="/" className={styles.edit}>
+            Edit this page
+          </Link>
+        )}
+        <Link to="/" className={styles.issue}>
+          Create an issue
+        </Link>
       </div>
     </div>
   </footer>
 );
+
+Footer.propTypes = {
+  showEdit: PropTypes.bool,
+};
+
+Footer.defaultProps = {
+  showEdit: true,
+};
 
 export default Footer;
