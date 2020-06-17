f8d8b70d9aacdfbd90133b76d7a615464bba692a
lbaker <lizbaker.pdx@gmail.com>
2020-06-17 16:59:09 -0700

    chore: Add breadcrumb context to explore data page

--- src/pages/explore-data.js
+++ src/pages/explore-data.js
@@ -1,3 +1,5 @@
+import { BreadcrumbContext } from '../components/BreadcrumbContext';
+import createBreadcrumbs from '../utils/create-breadcrumbs';
 import GuideListing from '../components/GuideListing/GuideListing';
 import GuideTile from '../components/GuideTile';
 import Layout from '../components/Layout';
@@ -5,6 +7,7 @@ import React from 'react';
 import SEO from '../components/Seo';
 import Section from '../components/Section';
 import styles from './explore-data.module.scss';
+import pages from '../data/sidenav.json';
 
 const heading = 'Get Data into New Relic';
 
@@ -34,21 +37,26 @@ const guides = [
   },
 ];
 
-const ExploreDataPage = () => (
-  <Layout>
-    <SEO title={heading} />
-    <Section backgroundBanner className={styles.backgroundBanner}>
-      <GuideListing>
-        <GuideListing.Heading>{heading}</GuideListing.Heading>
-        <GuideListing.Description>{description}</GuideListing.Description>
-        <GuideListing.List>
-          {guides.map((guide, index) => (
-            <GuideTile key={index} {...guide} />
-          ))}
-        </GuideListing.List>
-      </GuideListing>
-    </Section>
-  </Layout>
-);
+const ExploreDataPage = () => {
+  const crumbs = createBreadcrumbs('/explore-data', pages);
+  return (
+    <BreadcrumbContext.Provider value={crumbs}>
+      <Layout>
+        <SEO title={heading} />
+        <Section backgroundBanner className={styles.backgroundBanner}>
+          <GuideListing>
+            <GuideListing.Heading>{heading}</GuideListing.Heading>
+            <GuideListing.Description>{description}</GuideListing.Description>
+            <GuideListing.List>
+              {guides.map((guide, index) => (
+                <GuideTile key={index} {...guide} />
+              ))}
+            </GuideListing.List>
+          </GuideListing>
+        </Section>
+      </Layout>
+    </BreadcrumbContext.Provider>
+  );
+};
 
 export default ExploreDataPage;
