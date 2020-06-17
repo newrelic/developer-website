d24518e2439947526061c72efe4d8841badc57a0
Tim Glaser <tglaser@newrelic.com>
2020-06-16 18:37:18 -0700

    feat: Add menu button to mobile header

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import cx from 'classnames';
 import Navigation from './Navigation';
+import HamburgerMenu from './HamburgerMenu';
 import styles from './MobileHeader.module.scss';
 
-const MobileHeader = ({ className }) => (
-  <aside className={cx(styles.container, className)}>
-    <Link to="/" className={styles.logo} />
-    {/* <Navigation /> */}
-  </aside>
-);
+const MobileHeader = ({ className }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <aside className={cx(styles.container, className)}>
+      <Link to="/" className={styles.logo} />
+
+      <HamburgerMenu
+        className={styles.hamburgerMenu}
+        toggle={() => setIsOpen(!isOpen)}
+        isOpen={isOpen}
+      />
+
+      {/* <Navigation /> */}
+    </aside>
+  );
+};
 
 MobileHeader.propTypes = {
   className: PropTypes.string,
