0572f78d12267e80a521c3b5f7a3e0436b306ca2
Jerel Miller <jmiller@newrelic.com>
2020-06-16 21:54:52 -0700

    fix: Add back sidebar styles and logo component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import cx from 'classnames';
+import Logo from './Logo';
 import Navigation from './Navigation';
 import styles from './Sidebar.module.scss';
 
 const Sidebar = ({ className }) => (
   <aside className={cx(styles.sidebar, className)}>
-    <Link to="/" className={styles.logo} />
+    <Link to="/">
+      <Logo className={styles.logo} />
+    </Link>
     <Navigation />
   </aside>
 );
