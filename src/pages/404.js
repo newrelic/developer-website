c09f6c337d07c0fb381e942bff8d8587cdc7181f
Jerel Miller <jmiller@newrelic.com>
2020-06-16 21:00:49 -0700

    fix: Use own layout for 404 pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Layout from '../components/Layout';
 import SEO from '../components/Seo';
-import Section from '../components/Section';
+import SkewedContainer from '../components/SkewedContainer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import styles from './404.module.scss';
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404: Not found" />
-    <Section backgroundBanner className={styles.backgroundBanner}>
-      <div className={styles.messageContainer}>
-        <h1 className={styles.header}>404</h1>
-        <p>
-          This page does not exist. Check the URL and try again, or{' '}
-          <Link to="/">go home</Link>.
-        </p>
+    <div className={styles.layout}>
+      <Header />
+      <div className={styles.contentContainer}>
+        <SkewedContainer className={styles.content}>
+          <h1 className={styles.header}>404</h1>
+          <p>
+            This page does not exist. Check the URL and try again, or{' '}
+            <Link to="/">go home</Link>.
+          </p>
+        </SkewedContainer>
       </div>
-    </Section>
-  </Layout>
+      <Footer />
+    </div>
+  </>
 );
 
 export default NotFoundPage;
