f79be0649a11f1726b5da5e5d61100d9baf1bfaf
Jerel Miller <jmiller@newrelic.com>
2020-06-16 22:06:45 -0700

    feat: Use Logo component for mobile header

--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import cx from 'classnames';
+import Logo from './Logo';
 import Navigation from './Navigation';
 import HamburgerMenu from './HamburgerMenu';
 import styles from './MobileHeader.module.scss';
@@ -10,7 +11,9 @@ const MobileHeader = ({ className, isOpen, toggle }) => {
   return (
     <header className={cx(styles.container, className)}>
       <div className={styles.menuBar}>
-        <Link to="/" className={styles.logo} />
+        <Link to="/">
+          <Logo className={styles.logo} />
+        </Link>
 
         <HamburgerMenu
           className={styles.hamburgerMenu}
