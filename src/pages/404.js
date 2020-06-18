2a1ec43e818a1f26f3b07f32ab995969d212892e
Zack Stickles <zstickles@gmail.com>
2020-06-18 12:09:26 -0700

    chore: clean up imports

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import SEO from '../components/Seo';
 import SkewedContainer from '../components/SkewedContainer';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import Layout from '../components/Layout';
 import SEO from '../components/Seo';
