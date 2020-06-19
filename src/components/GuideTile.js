fca1d49d8011a9d8ab957dd96773b6874964072e
Zack Stickles <zstickles@gmail.com>
2020-06-19 13:47:52 -0700

    feat: added teh ability to specify icons to GuideTile

--- src/components/GuideTile.js
+++ src/components/GuideTile.js
@@ -1,11 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import FeatherIcon from './FeatherIcon';
 import cx from 'classnames';
 import { navigate } from 'gatsby';
 import styles from './GuideTile.module.scss';
 
-const GuideTile = ({ minutes, title, description, path, className }) => (
+const GuideTile = ({ icon, minutes, title, description, path, className }) => (
   <div className={cx(styles.tile, className)}>
+    {icon && (
+      <div className={styles.iconContainer}>
+        <FeatherIcon name={icon} size="2.5rem" />
+      </div>
+    )}
+
     <div className={styles.timeEstimate}>{minutes} minutes</div>
     <div className={styles.main}>
       <h2>{title}</h2>
@@ -23,6 +30,7 @@ GuideTile.propTypes = {
   description: PropTypes.string.isRequired,
   path: PropTypes.string.isRequired,
   className: PropTypes.string,
+  icon: PropTypes.string,
 };
 
 export default GuideTile;
