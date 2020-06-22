0b2dec41c820ed81d5a4c69a15263ef484bc4ecb
lbaker <lizbaker.pdx@gmail.com>
2020-06-22 14:14:38 -0700

    chore: First pass for code snippet line highlight

--- src/components/CodeSnippet.js
+++ src/components/CodeSnippet.js
@@ -6,11 +6,20 @@ import FeatherIcon from './FeatherIcon';
 import styles from './CodeSnippet.module.scss';
 import useClipboard from '../hooks/useClipboard';
 import useFormattedCode from '../hooks/useFormattedCode';
+import cx from 'classnames';
 
-const CodeSnippet = ({ children, copy, className, lineNumbers }) => {
+const CodeSnippet = ({
+  children,
+  copy,
+  className,
+  lineNumbers,
+  lineHighlight,
+}) => {
   const language = className.replace('language-', '');
   const formattedCode = useFormattedCode(children ?? '');
   const [copied, copyCode] = useClipboard();
+  const linesToHighlight =
+    lineHighlight && captureLinesToHighlight(lineHighlight.split(','));
 
   return (
     <div>
@@ -25,7 +34,16 @@ const CodeSnippet = ({ children, copy, className, lineNumbers }) => {
             <pre className={styles.codeContainer} style={style}>
               <code>
                 {tokens.map((line, i) => (
-                  <div key={i} {...getLineProps({ line, key: i })}>
+                  <div
+                    key={i}
+                    {...getLineProps({ line, key: i })}
+                    className={
+                      lineHighlight &&
+                      cx({
+                        [styles.highlight]: linesToHighlight.includes(i + 1),
+                      })
+                    }
+                  >
                     {lineNumbers !== 'false' && (
                       <span className={styles.lineNumber}>{i + 1}</span>
                     )}
@@ -51,11 +69,33 @@ const CodeSnippet = ({ children, copy, className, lineNumbers }) => {
   );
 };
 
+const captureLinesToHighlight = (lineArr) => {
+  const lineNumbers = lineArr
+    .filter((line) => !line.includes('-'))
+    .map((number) => Number(number));
+
+  const lineNumberSet = new Set(lineNumbers);
+
+  const ranges = lineArr.filter((line) => line.includes('-'));
+
+  ranges.forEach((range) => {
+    const rangeStart = Number(range[0]);
+    const rangeEnd = Number(range[2]);
+    const rangeArr = Array.from(
+      Array(rangeEnd - rangeStart + 1),
+      (_, i) => i + rangeStart
+    );
+    rangeArr.forEach((number) => lineNumberSet.add(number));
+  });
+  return Array.from(lineNumberSet);
+};
+
 CodeSnippet.propTypes = {
   children: PropTypes.node.isRequired,
   copy: PropTypes.string,
   className: PropTypes.string,
   lineNumbers: PropTypes.string,
+  lineHighlight: PropTypes.string,
 };
 
 CodeSnippet.defaultProps = {
