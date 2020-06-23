fcf7d1b917dcc53333ed803467f97b6c45515e66
Jerel Miller <jerelmiller@gmail.com>
2020-06-23 00:48:00 -0700

    Update component doc code snippets to handle dark theme

--- src/components/InlineCodeSnippet.js
+++ src/components/InlineCodeSnippet.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import github from 'prism-react-renderer/themes/github';
+import lightTheme from 'prism-react-renderer/themes/github';
+import darkTheme from 'prism-react-renderer/themes/nightOwl';
 import styles from './InlineCodeSnippet.module.scss';
 import cx from 'classnames';
+import useDarkMode from 'use-dark-mode';
 
 const InlineCodeSnippet = ({ children, language }) => {
+  const darkMode = useDarkMode();
+
   return (
     <Highlight
       {...defaultProps}
-      theme={github}
+      theme={darkMode.value ? darkTheme : lightTheme}
       code={children}
       language={language}
     >
