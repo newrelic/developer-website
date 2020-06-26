b693b7589da01a78cf56393c0ef42b188165c566
Jerel Miller <jerelmiller@gmail.com>
2020-06-25 17:24:19 -0700

    feat: Add the developer-champion page

---
+++ src/pages/developer-champion.js
@@ -0,0 +1,150 @@
+// ---
+// path: '/developer-champion'
+// title: 'New Relic Developer Champion'
+// template: 'GuideTemplate'
+// ---
+
+// ## What do developer champions do?
+// New Relic Champions demonstrate expertise in using New Relic products by solving large problems and positioning New Relic as a central force in their strategies. The New Relic Champions is a recognition and partnership program designed to acknowledge the developers that are driving innovation within their companies and making top contributions to the developer community. Also committing to making their work public by:
+
+// ### Open-source Contributions
+// Serving as an open-source author or maintainer for an accepted public project related to New Relic One
+
+// ### Sharing Ideas
+// Write posts in the New Relic Explorers Hub, or guides & "how-to" articles on the Developer website, or even as a guest author on our corporate blog.
+
+// ### Community Engagement
+// Delivering and/or organizing two events  based around an observability platform theme in which New Relic plays a crucial role
+// [Nominate a Developer Champion](https://forms.gle/Zkdub5e1x4MNqSKW9_)
+
+// ### Why should you join and how will we support?
+// As a benefit of being a Developer Champion, New Relic provides Champions unique access to our Developer Advocacy team and the resources of our product organization as well as specialized recognition and rewards.
+// ![](../../images/developer-champion/uptime-everything-header-image.jpg)
+
+// ### Developer Champions Benefits:
+// - Formal, specialized access to the New Relic Product organization
+//   -  Champions have direct access to the New Relic’s Developer Ecosystem team
+//   -  Custom badge to wear with pride at events
+// - Public recognition on the New Relic Developer website and badging in the New Relic Explorers Hub as a Champion
+// - Exclusive Champion-only SWAG
+// - Early access program for some of our products (under NDA)
+// - Priority access to offsite FutureHack events (including when Lew is participating)
+// - Increased Explorer’s Hub support SLA
+// - Access to private Developer Champion Explorer’s Hub group
+
+// [Nominate a Developer Champion](https://forms.gle/Zkdub5e1x4MNqSKW9_)
+
+// ![](../../images/developer-champion/colleagues-office.jpeg)
+// ![](../../images/developer-champion/team-meeting.jpg)
+
+import React from 'react';
+import cx from 'classnames';
+import Layout from '../components/Layout';
+import SEO from '../components/Seo';
+import PageTitle from '../components/PageTitle';
+import ExternalLink from '../components/ExternalLink';
+import colleaguesOffice from '../images/developer-champion/colleagues-office.jpeg';
+import devChampionHeader from '../images/developer-champion/developer-champion-header.jpg';
+import uptimeEverythingHeader from '../images/developer-champion/uptime-everything-header-image.jpg';
+import teamMeeting from '../images/developer-champion/team-meeting.jpg';
+import styles from './developer-champion.module.scss';
+
+const DeveloperChampionPage = () => {
+  return (
+    <Layout>
+      <SEO />
+      <PageTitle>New Relic Developer Champions</PageTitle>
+      <section className={cx(styles.section, styles.twoColumn)}>
+        <div>
+          <p>
+            New Relic Champions are the voice of the developer community. As
+            experts and innovators, they are given the resources to not only
+            share the newest product innovations and updates but also provide
+            feedback of the community back to New Relic product and engineering
+            teams.
+          </p>
+          <p>
+            Champions solve big problems using New Relic as their linchpin and
+            are recognized as experts and leaders in the New Relic technical
+            community.{' '}
+          </p>
+          <ExternalLink href="https://forms.gle/Zkdub5e1x4MNqSKW9">
+            Nominate a Developer Champion
+          </ExternalLink>
+        </div>
+        <img
+          className={styles.img}
+          src={devChampionHeader}
+          alt="developer champion header"
+        />
+      </section>
+      <section className={styles.section}>
+        <h2>What do developer champions do?</h2>
+        <p>
+          New Relic Champions demonstrate expertise in using New Relic products
+          by solving large problems and positioning New Relic as a central force
+          in their strategies. The New Relic Champions is a recognition and
+          partnership program designed to acknowledge the developers that are
+          driving innovation within their companies and making top contributions
+          to the developer community. Also committing to making their work
+          public by:
+        </p>
+      </section>
+      <section className={cx(styles.section, styles.twoColumn)}>
+        <img
+          className={styles.img}
+          src={uptimeEverythingHeader}
+          alt="uptime everything header"
+        />
+        <div>
+          <h2>Why should you join and how will we support?</h2>
+          <p>
+            As a benefit of being a Developer Champion, New Relic provides
+            Champions unique access to our Developer Advocacy team and the
+            resources of our product organization as well as specialized
+            recognition and rewards.
+          </p>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <h2>Developer Champions Benefits:</h2>
+        <ul>
+          <li>
+            Formal, specialized access to the New Relic Product organization
+            <ul>
+              <li>
+                Champions have direct access to the New Relic’s Developer
+                Ecosystem team
+              </li>
+              <li>Custom badge to wear with pride at events</li>
+            </ul>
+          </li>
+          <li>
+            Public recognition on the New Relic Developer website and badging in
+            the New Relic Explorers Hub as a Champion
+          </li>
+          <li>Exclusive Champion-only SWAG</li>
+          <li>Early access program for some of our products (under NDA)</li>
+          <li>
+            Priority access to offsite FutureHack events (including when Lew is
+            participating)
+          </li>
+          <li>
+            Increased Explorer’s Hub support SLA Access to private Developer
+            Champion Explorer’s Hub group
+          </li>
+        </ul>
+      </section>
+      <section className={cx(styles.section, styles.twoColumn)}>
+        <img
+          src={colleaguesOffice}
+          className={styles.img}
+          alt="colleagues office"
+        />
+        <img src={teamMeeting} className={styles.img} alt="team meeting" />
+      </section>
+    </Layout>
+  );
+};
+
+export default DeveloperChampionPage;
