528d3715da34050f1e7932bdee80724c5ad5a444
Jerel Miller <jerelmiller@gmail.com>
2020-06-27 21:21:59 -0700

    feat: Create a button component

---
+++ src/components/Button.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import styles from './Button.module.scss';
+
+const Button = ({
+  as: Component = 'button',
+  children,
+  className,
+  variant,
+  ...props
+}) => (
+  <Component
+    {...props}
+    className={cx(className, styles.button, styles[variant])}
+  >
+    {children}
+  </Component>
+);
+
+Button.VARIANT = {
+  PRIMARY: 'primary',
+  NORMAL: 'normal',
+};
+
+Button.propTypes = {
+  as: PropTypes.elementType,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  variant: PropTypes.oneOf(Object.values(Button.VARIANT)).isRequired,
+};
+
+export default Button;
