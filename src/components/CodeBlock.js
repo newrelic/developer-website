b61401aee5512d150637fd0ff1ea7182a06b48e2
Jerel Miller <jerelmiller@gmail.com>
2020-07-04 01:08:56 -0700

    chore: Add code block component

---
+++ src/components/CodeBlock.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Highlight from 'prism-react-renderer';
+import Prism from 'prismjs';
+import styles from './CodeBlock.module.scss';
+
+const CodeBlock = ({ children, language }) => {
+  return (
+    <Highlight Prism={Prism} code={children.trim()} language={language}>
+      {({ tokens, getLineProps, getTokenProps }) => (
+        <pre className={styles.container}>
+          <code>
+            {tokens.map((line, i) => (
+              // eslint-disable-next-line react/jsx-key
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  // eslint-disable-next-line react/jsx-key
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </code>
+        </pre>
+      )}
+    </Highlight>
+  );
+};
+
+CodeBlock.propTypes = {
+  children: PropTypes.string.isRequired,
+  language: PropTypes.string,
+  lineNumbers: PropTypes.bool,
+};
+
+CodeBlock.defaultProps = {
+  lineNumbers: false,
+};
+
+export default CodeBlock;
