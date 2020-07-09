1afbe292c25c5439a780a1906ef05e3aefd2e012
Jerel Miller <jerelmiller@gmail.com>
2020-07-08 23:13:12 -0700

    refactor: switch to use emotion for the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
-import styles from './Button.module.scss';
+import { css } from '@emotion/core';
+
+const VARIANTS = {
+  PLAIN: 'plain',
+  PRIMARY: 'primary',
+  NORMAL: 'normal',
+};
+
+const SIZES = {
+  SMALL: 'small',
+};
+
+const styles = {
+  size: {
+    [SIZES.SMALL]: css`
+      font-size: 0.75rem;
+    `,
+  },
+  variant: {
+    [VARIANTS.PRIMARY]: css`
+      color: var(--color-white);
+      border-color: var(--color-brand-800);
+      background-color: var(--color-brand-800);
+
+      .dark-mode & {
+        color: var(--primary-background-color);
+        background-color: var(--color-brand-400);
+        border-color: var(--color-brand-400);
+      }
+    `,
+    [VARIANTS.NORMAL]: css`
+      color: var(--color-neutrals-800);
+      border-color: var(--color-neutrals-100);
+      background-color: var(--color-neutrals-100);
+
+      .dark-mode & {
+        color: var(--color-white);
+        border-color: var(--color-dark-100);
+        background-color: var(--color-dark-100);
+      }
+    `,
+    [VARIANTS.PLAIN]: css`
+      color: var(--color-brand-800);
+      border-color: transparent;
+      background-color: transparent;
+
+      .dark-mode & {
+        color: var(--color-brand-400);
+      }
+    `,
+  },
+};
 
 const Button = ({
   as: Component = 'button',
@@ -13,21 +63,31 @@ const Button = ({
 }) => (
   <Component
     {...props}
-    className={cx(className, styles.button, styles[variant], styles[size])}
+    className={className}
+    css={css`
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      padding: 0.5rem 1rem;
+      font-size: 0.875rem;
+      font-weight: 600;
+      border-radius: 3px;
+      font-family: var(--primary-font-family);
+      line-height: 1;
+      cursor: pointer;
+      border-width: 1px;
+      border-style: solid;
+
+      ${styles.variant[variant]}
+      ${styles.size[size]}
+    `}
   >
     {children}
   </Component>
 );
 
-Button.VARIANT = {
-  PLAIN: 'plain',
-  PRIMARY: 'primary',
-  NORMAL: 'normal',
-};
-
-Button.SIZE = {
-  SMALL: 'small',
-};
+Button.VARIANT = VARIANTS;
+Button.SIZE = SIZES;
 
 Button.propTypes = {
   as: PropTypes.elementType,
