4f534b56dc14788ef5762caad8ad81633caa7ec4
lbaker <lizbaker.pdx@gmail.com>
2020-07-13 11:34:39 -0700

    chore: Conditionally insert GA via helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
+import { Helmet } from 'react-helmet';
+import Cookies from 'js-cookie';
 import Footer from './Footer';
 import GlobalHeader from './GlobalHeader';
 import MobileHeader from './MobileHeader';
@@ -12,11 +14,34 @@ import 'normalize.css';
 import './styles.scss';
 import '../theme.scss';
 
+const gaTrackingId = 'UA-3047412-33';
+const gdprConsentCookieName = 'newrelic-gdpr-consent';
+
 const Layout = ({ children }) => {
+  const [cookieConsent, setCookieConsent] = useState(false);
   const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  useEffect(() => {
+    const consentValue = Cookies.get(gdprConsentCookieName);
+    consentValue && setCookieConsent(true);
+  }, []);
 
   return (
     <div className={styles.layout}>
+      <Helmet>
+        {cookieConsent ? (
+          <script>
+            {`(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          
+          ga('create', '${gaTrackingId}', 'auto');
+          ga('set', 'anonymizeIp', true);
+          ga('send', 'pageview');`}
+          </script>
+        ) : null}
+      </Helmet>
       <GlobalHeader />
       <MobileHeader
         className={styles.hideOnDesktop}
@@ -35,7 +60,7 @@ const Layout = ({ children }) => {
           <Footer className={styles.footer} />
         </div>
       </div>
-      <CookieApprovalDialog />
+      <CookieApprovalDialog setCookieConsent={setCookieConsent} />
     </div>
   );
 };
