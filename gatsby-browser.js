e9a828a2d3287855ed781cbc9c252453633920b3
lbaker <lizbaker.pdx@gmail.com>
2020-07-13 11:36:01 -0700

    chore: Add GA tracking on page update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,24 @@
 
 import wrapPageElement from './gatsby/wrap-page-element';
 
-export { wrapPageElement };
+const onRouteUpdate = ({ location }) => {
+  if (process.env.NODE_ENV !== `production` || typeof ga !== `function`) {
+    return null;
+  }
+
+  // wrap inside a timeout to make sure react-helmet is done with it's changes (https://github.com/gatsbyjs/gatsby/issues/9139)
+  // reactHelmet is using requestAnimationFrame: https://github.com/nfl/react-helmet/blob/5.2.0/src/HelmetUtils.js#L296-L299
+  const sendPageView = () => {
+    const pagePath = location
+      ? location.pathname + location.search + location.hash
+      : undefined;
+    window.ga(`set`, `page`, pagePath);
+    window.ga(`send`, `pageview`);
+  };
+
+  // Minimum delay for reactHelmet's requestAnimationFrame
+  setTimeout(sendPageView, 32);
+
+  return null;
+};
+export { onRouteUpdate, wrapPageElement };
