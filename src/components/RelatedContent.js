1d183b92c63b0465cb1814759df70fbe97d6b45b
Jerel Miller <jerelmiller@gmail.com>
2020-07-29 14:08:44 -0700

    feat: create a related content component

---
+++ src/components/RelatedContent.js
@@ -0,0 +1,70 @@
+import React, { useContext } from 'react';
+import { css } from '@emotion/core';
+import { graphql, useStaticQuery } from 'gatsby';
+import { Button, ExternalLink, Icon } from '@newrelic/gatsby-theme-newrelic';
+import { PageContext } from './PageContext';
+
+const RelatedContent = ({ className }) => {
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          repository
+        }
+        layout {
+          contentPadding
+        }
+      }
+    }
+  `);
+  const { fileRelativePath } = useContext(PageContext);
+
+  const {
+    layout,
+    siteMetadata: { repository },
+  } = site;
+
+  return (
+    <aside
+      className={className}
+      css={css`
+        padding: 0 ${layout.contentPadding};
+      `}
+    >
+      <h4>Contribute</h4>
+      <Button
+        as={ExternalLink}
+        href={`${repository}/issues/new/choose`}
+        css={css`
+          margin-right: 0.5rem;
+        `}
+        variant={Button.VARIANT.PRIMARY}
+        size={Button.SIZE.SMALL}
+      >
+        <Icon
+          name={Icon.TYPE.GITHUB}
+          css={css`
+            margin-right: 0.5rem;
+          `}
+        />
+        File an issue
+      </Button>
+      <Button
+        as={ExternalLink}
+        href={`${repository}/tree/main/${fileRelativePath}`}
+        variant={Button.VARIANT.PLAIN}
+        size={Button.SIZE.SMALL}
+      >
+        <Icon
+          name={Icon.TYPE.EDIT}
+          css={css`
+            margin-right: 0.5rem;
+          `}
+        />
+        Edit this page
+      </Button>
+    </aside>
+  );
+};
+
+export default RelatedContent;
