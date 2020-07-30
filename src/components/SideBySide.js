c0277d9ba1cb4e9af52aa70d1ba606be92050e4d
Jerel Miller <jerelmiller@gmail.com>
2020-07-30 12:50:29 -0700

    feat: use dynamic breakpoint for side by side component based on layout

--- src/components/SideBySide.js
+++ src/components/SideBySide.js
@@ -1,10 +1,17 @@
 import React, { Children, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import { css } from '@emotion/core';
 import styles from './SideBySide.module.scss';
 import splitUsing from '../utils/splitUsing';
 import splitWhen from '../utils/splitWhen';
 import { isMdxType } from '../utils/mdx';
+import usePageLayout from '../hooks/usePageLayout';
+
+const BREAKPOINTS = {
+  SINGLE_COLUMN: '1240px',
+  RELATED_CONTENT: '1520px',
+};
 
 const SideBySide = ({ className, children, type }) => {
   const types = Array.isArray(type) ? type : [type];
@@ -32,8 +39,18 @@ const SideBySide = ({ className, children, type }) => {
     )
   );
 
+  const pageLayout = usePageLayout();
+
   return (
-    <div className={cx(className, styles.container)}>
+    <div
+      css={css`
+        @media (max-width: ${BREAKPOINTS[pageLayout]}) {
+          grid-template-columns: minmax(0, 1fr);
+          grid-gap: 0;
+        }
+      `}
+      className={cx(className, styles.container)}
+    >
       {sections.map(([left, right], idx) => (
         <Fragment key={idx}>
           <div className={cx({ [styles.spanColumns]: !rendersRightColumn })}>
