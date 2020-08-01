2bb8aecad26635d6310252dcf603245b0a4672c4
Jerel Miller <jerelmiller@gmail.com>
2020-07-31 23:13:02 -0700

    refactor: make modules a prop to RelatedContent and update the guide template

--- src/components/PageLayout/RelatedContent.js
+++ src/components/PageLayout/RelatedContent.js
@@ -1,40 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
-import { graphql } from 'gatsby';
 
-import { Contribute, PageUpdated, Resources } from '../RelatedContentModules';
-
-const RelatedContent = ({ page }) => {
-  return (
-    <aside
-      data-swiftype-index={false}
-      css={css`
-        grid-area: related-content;
-        position: sticky;
-        top: calc(var(--global-header-height) + 2rem);
-        align-self: start;
-        padding: 1rem;
-        border: 1px solid var(--divider-color);
-        border-radius: 0.25rem;
-      `}
-    >
-      <Contribute />
-      <Resources page={page} />
-      <PageUpdated page={page} />
-    </aside>
-  );
-};
+const RelatedContent = ({ page, modules }) => (
+  <aside
+    data-swiftype-index={false}
+    css={css`
+      grid-area: related-content;
+      position: sticky;
+      top: calc(var(--global-header-height) + 2rem);
+      align-self: start;
+      padding: 1rem;
+      border: 1px solid var(--divider-color);
+      border-radius: 0.25rem;
+    `}
+  >
+    {modules.map((Module, idx) => (
+      <Module key={idx} page={page} />
+    ))}
+  </aside>
+);
 
 RelatedContent.propTypes = {
+  modules: PropTypes.arrayOf(PropTypes.elementType).isRequired,
   page: PropTypes.object.isRequired,
 };
-
-export const query = graphql`
-  fragment RelatedContent_page on Mdx {
-    ...Resources_page
-    ...PageUpdated_page
-  }
-`;
 
 export default RelatedContent;
