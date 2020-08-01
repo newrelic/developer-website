483c55a184eea23c5b6944e76e00d70fd0b65f0a
Jerel Miller <jerelmiller@gmail.com>
2020-07-31 23:28:27 -0700

    chore: fix lint error for prop type validation

--- src/components/RelatedContentModules/Title.js
+++ src/components/RelatedContentModules/Title.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Title = ({ children }) => <h4>{children}</h4>;
+
+Title.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Title;
