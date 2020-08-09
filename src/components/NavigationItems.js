de74639a6aa16cda716b5e18d94e212854d9aff4
Jerel Miller <jerelmiller@gmail.com>
2020-08-08 22:48:14 -0700

    fix: dont open links on home page

--- src/components/NavigationItems.js
+++ src/components/NavigationItems.js
@@ -96,19 +96,17 @@ const NavIcon = ({ page }) => {
 const NavItem = ({ page, depthLevel, searchTerm, filteredPageNames }) => {
   const location = useLocation();
   const crumbs = useContext(BreadcrumbContext).flatMap((x) => x.displayName);
-  const isHomepage = location.pathname === '/';
   const isBreadCrumb = crumbs.includes(page.displayName);
   const matchesSearch = filteredPageNames?.includes(page.displayName);
   const hasChangedPage = location.pathname !== usePrevious(location.pathname);
-  const shouldExpand = (isHomepage && depthLevel === 0) || isBreadCrumb;
-  const [toggleIsExpanded, setToggleIsExpanded] = useState(shouldExpand);
+  const [toggleIsExpanded, setToggleIsExpanded] = useState(isBreadCrumb);
   const isExpanded = toggleIsExpanded || matchesSearch;
 
   useEffect(() => {
     if (hasChangedPage) {
-      setToggleIsExpanded(shouldExpand);
+      setToggleIsExpanded(isBreadCrumb);
     }
-  }, [hasChangedPage, shouldExpand]);
+  }, [hasChangedPage, isBreadCrumb]);
 
   const isCurrentPage =
     normalizeUrl(location.pathname) === normalizeUrl(page.url);
