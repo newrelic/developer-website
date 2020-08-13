82323323fbd57a413da9a419d317525b9852871f
Jerel Miller <jerelmiller@gmail.com>
2020-08-12 21:46:28 -0700

    feat: fetch results from swiftype for the node and save the data

--- plugins/gatsby-source-swiftype/gatsby-node.js
+++ plugins/gatsby-source-swiftype/gatsby-node.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const fetch = require('node-fetch');
 const { appendTrailingSlash, stripTrailingSlash } = require('./src/utils/url');
 
 const data = {};
@@ -28,24 +29,35 @@ exports.onCreateNode = async ({ node, getNodesByType }, pluginOptions) => {
 
   const { page: pageFilters = {} } = params.filters || {};
 
-  const allParams = {
-    ...params,
-    engine_key: engineKey,
-    per_page: pageLimit,
-    filters: {
-      ...params.filters,
-      page: {
-        ...pageFilters,
-        url: [
-          `!${appendTrailingSlash(url)}`,
-          `!${stripTrailingSlash(url)}`,
-          ...(pageFilters.url || []),
-        ],
+  const result = await fetch(
+    'https://search-api.swiftype.com/api/v1/public/engines/search.json',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    },
-  };
+      body: JSON.stringify({
+        ...params,
+        engine_key: engineKey,
+        per_page: pageLimit,
+        filters: {
+          ...params.filters,
+          page: {
+            ...pageFilters,
+            url: [
+              `!${appendTrailingSlash(url)}`,
+              `!${stripTrailingSlash(url)}`,
+              ...(pageFilters.url || []),
+            ],
+          },
+        },
+      }),
+    }
+  );
 
-  data[pathname] = allParams;
+  const { records } = await result.json();
+
+  data[pathname] = records.page;
 };
 
 exports.onPostBootstrap = (_, pluginOptions) => {
