3f56794abc069ce989af6746ed2b7cb38cfbfab3
Jerel Miller <jerelmiller@gmail.com>
2020-08-12 23:22:58 -0700

    chore: read/write to the json file in onCreateNode

--- plugins/gatsby-source-swiftype/gatsby-node.js
+++ plugins/gatsby-source-swiftype/gatsby-node.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const search = require('./src/search');
 
-const data = {};
-
 exports.onCreateNode = async ({ node, getNodesByType }, pluginOptions) => {
   const {
     enabled,
@@ -11,6 +9,7 @@ exports.onCreateNode = async ({ node, getNodesByType }, pluginOptions) => {
     getPath,
     pageLimit,
     engineKey,
+    file,
   } = pluginOptions;
 
   if (!enabled || !filterNode({ node })) {
@@ -23,6 +22,7 @@ exports.onCreateNode = async ({ node, getNodesByType }, pluginOptions) => {
     },
   ] = getNodesByType('Site');
 
+  const data = JSON.parse(fs.readFileSync(file));
   const params = getParams({ node });
   const pathname = getPath({ node });
 
@@ -31,13 +31,8 @@ exports.onCreateNode = async ({ node, getNodesByType }, pluginOptions) => {
     pageLimit,
   });
 
-  data[pathname] = result;
-};
-
-exports.onPostBootstrap = (_, pluginOptions) => {
-  const { file, enabled } = pluginOptions;
-
-  if (enabled) {
-    fs.writeFileSync(file, JSON.stringify(data, null, 2), { flag: 'w' });
-  }
+  fs.writeFileSync(
+    file,
+    JSON.stringify({ ...data, [pathname]: result }, null, 2, { flag: 'w' })
+  );
 };
