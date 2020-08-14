ae35e06f73a4f79fc667a31e28ffe4506f8e6910
John P Vajda <jvajda@newrelic.com>
2020-08-13 18:10:44 -0600

    added the right rail contribute link

--- src/components/RelatedContentModules/Contribute.js
+++ src/components/RelatedContentModules/Contribute.js
@@ -26,7 +26,8 @@ const Contribute = ({ pageContext }) => {
   return (
     <Section
       css={css`
-        background-color: #edeeee`}>
+          background-color: #edeeee;
+        `}>
       <Title>Contribute</Title>
       <Button
         as={ExternalLink}
@@ -60,9 +61,9 @@ const Contribute = ({ pageContext }) => {
         Edit this page
       </Button>
       <p css={css`
-        margin-top: 0.5rem;
-        font-size: 0.8rem;
-        display: block;
+          margin-top: 0.5rem;
+          font-size: 0.8rem;
+          display: block;
       `}>
         Read our <a href="https://github.com/newrelic/developer-website/blob/main/CONTRIBUTING.md">guide</a> on how to contribute</p>
     </Section>
