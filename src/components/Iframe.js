72924f2d88108a65a1868c3034119b8df9b0735d
Zack Stickles <zstickles@gmail.com>
2020-08-14 14:20:20 -0700

    chore: accessability prop updates

--- src/components/Iframe.js
+++ src/components/Iframe.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 
 const Iframe = (props) => {
   // strip out width and then render iframe
-  const { width, ...limitedProps } = props;
+  // eslint-disable-next-line no-unused-vars
+  const { width, title, ...limitedProps } = props;
 
   return (
     <iframe
+      title={title}
       {...limitedProps}
       css={css`
         width: 100%;
       `}
     />
   );
+};
+
+Iframe.propTypes = {
+  width: PropTypes.string,
+  title: PropTypes.string,
+};
+
+Iframe.defaultProps = {
+  title: 'iFrame content',
 };
 
 export default Iframe;
