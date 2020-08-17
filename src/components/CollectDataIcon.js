68938516c85312798c987d0f0f8cc87b58d1bfec
Jerel Miller <jerelmiller@gmail.com>
2020-08-17 15:42:22 -0700

    chore: update to new icon

--- src/components/CollectDataIcon.js
+++ src/components/CollectDataIcon.js
@@ -6,7 +6,6 @@ const styles = {
   stem: css`
     fill: none;
     stroke: currentColor;
-    stroke-width: 0.5;
     stroke-linecap: square;
     stroke-linejoin: round;
   `,
@@ -18,7 +17,6 @@ const styles = {
   main: css`
     fill: none;
     stroke: currentColor;
-    stroke-width: 0.5;
     stroke-linecap: round;
     stroke-linejoin: round;
   `,
@@ -39,55 +37,49 @@ const CollectDataIcon = ({ className, size }) => (
     `}
   >
     <g>
-      <g transform="translate(-76 -677)">
-        <g transform="translate(78 679)">
-          <path d="M11.6 1.9h-1.4" css={styles.stem} />
-          <g>
-            <path
-              d="M13 1.7c0-.4-.3-.7-.7-.7-.4 0-.7.3-.7.7s.3.7.7.7c.4 0 .7-.3.7-.7"
-              css={styles.stemOutline}
-            />
-            <path
-              d="M13 1.7c0-.4-.3-.7-.7-.7-.4 0-.7.3-.7.7s.3.7.7.7c.4 0 .7-.3.7-.7z"
-              css={styles.main}
-            />
-            <path d="M11.7 10.1h-1.6" css={styles.stem} />
-            <path
-              d="M13 10.1c0-.4-.3-.7-.7-.7-.4 0-.7.3-.7.7s.3.7.7.7c.4 0 .7-.3.7-.7"
-              css={styles.stemOutline}
-            />
-            <path
-              d="M13 10.1c0-.4-.3-.7-.7-.7-.4 0-.7.3-.7.7s.3.7.7.7c.4 0 .7-.3.7-.7z"
-              css={styles.main}
-            />
-            <path d="M.3 6.1h1.6" css={styles.stem} />
-            <path
-              d="M-1 6.1c0 .4.3.7.7.7.4 0 .7-.3.7-.7s-.4-.7-.7-.7c-.4 0-.7.3-.7.7"
-              css={styles.stemOutline}
-            />
-            <path
-              d="M-1 6.1c0 .4.3.7.7.7.4 0 .7-.3.7-.7s-.4-.7-.7-.7c-.4 0-.7.3-.7.7z"
-              css={styles.main}
-            />
-            <path
-              d="M2 0v3c0 .6 1.6 1.4 4 1.4 2.5 0 4-.8 4-1.4V0"
-              css={styles.main}
-            />
-            <path
-              d="M6.1 8.6c2.5 0 4-.8 4-1.4V4.3c-.9.6-2.5 1-4.1 1-1.5 0-3.1-.3-4-1v3c0 .5 1.6 1.3 4.1 1.3z"
-              css={styles.main}
-            />
-            <path
-              d="M6.1 9.6c-1.6 0-3.2-.3-4.1-1V11c0 1.3 2.1 2 4.1 2 2 0 4.1-.7 4.1-2V8.6c-1 .7-2.6 1-4.1 1z"
-              css={styles.main}
-            />
-            <path
-              d="M10.1 0c0 .6-1.8 1-4.1 1S2 .6 2 0s1.8-1 4.1-1 4 .5 4 1z"
-              css={styles.main}
-            />
-          </g>
-        </g>
-      </g>
+      <path css={styles.stem} d="M13.6 3.8h-1.4" />
+      <path
+        css={styles.stemOutline}
+        d="M15 3.8c0-.4-.3-.7-.7-.7s-.7.3-.7.7.3.7.7.7.7-.3.7-.7"
+      />
+      <path
+        css={styles.main}
+        d="M15 3.8c0-.4-.3-.7-.7-.7s-.7.3-.7.7.3.7.7.7.7-.3.7-.7z"
+      />
+      <path css={styles.stem} d="M13.8 12.2h-1.6" />
+      <path
+        css={styles.stemOutline}
+        d="M15 12.2c0-.4-.3-.7-.7-.7s-.7.3-.7.7.3.7.7.7.7-.3.7-.7"
+      />
+      <path
+        css={styles.main}
+        d="M15 12.2c0-.4-.3-.7-.7-.7s-.7.3-.7.7.3.7.7.7.7-.3.7-.7z"
+      />
+      <path css={styles.stem} d="M2.3 8.2h1.6" />
+      <path
+        css={styles.stemOutline}
+        d="M1 8.2c0 .4.3.7.7.7s.7-.3.7-.7c0-.4-.4-.7-.7-.7-.4 0-.7.3-.7.7"
+      />
+      <path
+        css={styles.main}
+        d="M1 8.2c0 .4.3.7.7.7s.7-.3.7-.7c0-.4-.4-.7-.7-.7-.4 0-.7.3-.7.7z"
+      />
+      <path
+        css={styles.main}
+        d="M4 1.6v3C4 5.2 5.6 6 8 6c2.5 0 4-.8 4-1.4v-3"
+      />
+      <path
+        css={styles.main}
+        d="M8.1 10.7c2.5 0 4-.8 4-1.4V6.4c-.9.6-2.5 1-4.1 1-1.5 0-3.1-.3-4-1v3c0 .5 1.6 1.3 4.1 1.3z"
+      />
+      <path
+        css={styles.main}
+        d="M8.1 12.1c-1.6 0-3.2-.3-4.1-1v2.4c0 1.3 2.1 2 4.1 2s4.1-.7 4.1-2v-2.4c-1 .7-2.6 1-4.1 1z"
+      />
+      <path
+        css={styles.main}
+        d="M12.1 1.6c0 .6-1.8 1-4.1 1s-4-.4-4-1 1.8-1 4.1-1 4 .5 4 1z"
+      />
     </g>
   </svg>
 );
