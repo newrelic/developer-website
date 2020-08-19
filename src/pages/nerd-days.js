e6636f485dd10ce2534363cb03c4767055c3c319
Jemiah Sius <jsius@newrelic.com>
2020-08-19 16:05:52 -0700

    fix: update nerd days page to use PageLayout

--- src/pages/nerd-days.js
+++ src/pages/nerd-days.js
@@ -1,9 +1,8 @@
 import React, { useEffect } from 'react';
 import { Link } from 'gatsby';
 import cx from 'classnames';
-import Layout from '../components/Layout';
+import PageLayout from '../components/PageLayout';
 import SEO from '../components/Seo';
-import PageTitle from '../components/PageTitle';
 import nerdDays from '../images/nerd-days/nerd-days.png';
 import styles from './nerd-days.module.scss';
 import '../components/marketo.scss';
@@ -47,46 +46,55 @@ const NerdDaysPage = () => {
   }, []);
 
   return (
-    <Layout>
+    <>
       <SEO />
-      <PageTitle>New Relic Nerd Days</PageTitle>
-      <section className={cx(styles.section, styles.twoColumnAlt)}>
-        <div>
-          <img className={styles.img} src={nerdDays} alt="nerd days header" />
-          <p>
-            Nerd Days is a FREE engineering conference that kicks off October 13{' '}
-            <em>(Dates vary by region)</em>. Focused on building more perfect
-            software, our goal is to spend less time looking at slides that tell
-            you what software can do and more time on getting your hands on the
-            software to solve problems efficiently.
-          </p>
-          <p>
-            You’ll hear from fellow engineers who built New Relic solutions and
-            New Relic users from various industries. Whether you’re new or a
-            data nerd, there’s an interactive session for you.
-          </p>
-          <p>We look forward to building with you during Nerd Days! </p>
-          <h2 className={styles.h2}>CALL FOR PROPOSALS</h2>
-          <p>
-            Got an interesting topic you want to share with your fellow
-            engineers?{' '}
-            <Link to="https://docs.google.com/forms/d/e/1FAIpQLSe8LOilCrD_TCUPyHFHG_QzVW2UdUR0UKZ8H8WNylz0flB7OQ/viewform">
-              Submit your proposals
-            </Link>{' '}
-            by September 1, 2021 at 11:59 PM PT. For more details, check out the
-            one-pager. proposals will contacted by September 30, 2021 at the
-            latest.
-          </p>
-        </div>
-        <div className={styles.formholder}>
-          <div className={styles.form}>
-            <p className={styles.formhead}>REGISTER FOR THIS NERD DAYS</p>
-            <form id="mktoForm_3525" />
-          </div>
-        </div>
-      </section>
-      <section />
-    </Layout>
+      <PageLayout type={PageLayout.TYPE.SINGLE_COLUMN}>
+        <PageLayout.Header title="New Relic Nerd Days" />
+        <PageLayout.Content>
+          <section className={cx(styles.section, styles.twoColumnAlt)}>
+            <div>
+              <img
+                className={styles.img}
+                src={nerdDays}
+                alt="nerd days header"
+              />
+              <p>
+                Nerd Days is a FREE engineering conference that kicks off
+                October 13 <em>(Dates vary by region)</em>. Focused on building
+                more perfect software, our goal is to spend less time looking at
+                slides that tell you what software can do and more time on
+                getting your hands on the software to solve problems
+                efficiently.
+              </p>
+              <p>
+                You’ll hear from fellow engineers who built New Relic solutions
+                and New Relic users from various industries. Whether you’re new
+                or a data nerd, there’s an interactive session for you.
+              </p>
+              <p>We look forward to building with you during Nerd Days! </p>
+              <h2 className={styles.h2}>CALL FOR PROPOSALS</h2>
+              <p>
+                Got an interesting topic you want to share with your fellow
+                engineers?{' '}
+                <Link to="https://docs.google.com/forms/d/e/1FAIpQLSe8LOilCrD_TCUPyHFHG_QzVW2UdUR0UKZ8H8WNylz0flB7OQ/viewform">
+                  Submit your proposals
+                </Link>{' '}
+                by September 1, 2021 at 11:59 PM PT. For more details, check out
+                the one-pager. proposals will contacted by September 30, 2021 at
+                the latest.
+              </p>
+            </div>
+            <div className={styles.formholder}>
+              <div className={styles.form}>
+                <p className={styles.formhead}>REGISTER FOR THIS NERD DAYS</p>
+                <form id="mktoForm_3525" />
+              </div>
+            </div>
+          </section>
+          <section />
+        </PageLayout.Content>
+      </PageLayout>
+    </>
   );
 };
 
