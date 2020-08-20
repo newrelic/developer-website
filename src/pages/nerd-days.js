7e823e50109d039dd8ac54e91f10fad34b3adead
veextee <vi.tranx@gmail.com>
2020-08-19 17:04:55 -0700

    Update nerd-days.js

--- src/pages/nerd-days.js
+++ src/pages/nerd-days.js
@@ -79,8 +79,7 @@ const NerdDaysPage = () => {
                 <Link to="https://docs.google.com/forms/d/e/1FAIpQLSe8LOilCrD_TCUPyHFHG_QzVW2UdUR0UKZ8H8WNylz0flB7OQ/viewform">
                   Submit your proposals
                 </Link>{' '}
-                by September 1, 2020 at 11:59 PM PT. For more details, check out
-                the one-pager. proposals will contacted by September 30, 2020 at
+                by September 1, 2020 at 11:59 PM PT. Accepted proposals will contacted by September 30, 2020 at
                 the latest.
               </p>
             </div>
