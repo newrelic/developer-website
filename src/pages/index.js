7d122a3f134f89681d0290b7c2ef3a35c64dfe68
Cayla Hamann <chamann@newrelic.com>
2020-08-21 17:52:17 -0400

    fix: image re resizes to browser

--- src/pages/index.js
+++ src/pages/index.js
@@ -77,15 +77,14 @@ const IndexPage = ({ data, pageContext }) => {
                 <Link to="/nerd-days">Learn more</Link>
               </p>
             </div>
-
-            <img
-              src={nerdDays}
-              alt="nerd days header"
+            <div
               css={css`
                 flex: 1;
                 margin-top: 0;
               `}
-            />
+            >
+              <img src={nerdDays} alt="nerd days header" />
+            </div>
           </section>
 
           <section className={cx(styles.section, styles.stripedSection)}>
