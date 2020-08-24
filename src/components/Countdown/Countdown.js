a92f2e8d840132cbcabfb3d7ffd2d8adca589ab9
Maria Shimkovska <mshimkov@stetson.edu>
2020-08-23 19:52:42 -0700

    fixinf errors caused by prettier

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import CountdownUnit from './CountdownUnit';
-import styles from './CountdownContainer.module.scss'
+import PropTypes from 'prop-types';
+import styles from './CountdownContainer.module.scss';
 
 const Countdown = ({ countdown }) => {
-    return (
-        <div className={styles.countdown}>
-            <CountdownUnit unit={countdown.days} label='Days' />
-            <h2>:</h2>
-            <CountdownUnit unit={countdown.hours} label='Hours' />
-            <h2>:</h2>
-            <CountdownUnit unit={countdown.minutes} label='Minutes' />
-            <h2>:</h2>
-            <CountdownUnit unit={countdown.seconds} label='Seconds' />
-        </div>
-    );
+  return (
+    <div className={styles.countdown}>
+      <CountdownUnit unit={countdown.days} label='Days' />
+      <h2>:</h2>
+      <CountdownUnit unit={countdown.hours} label='Hours' />
+      <h2>:</h2>
+      <CountdownUnit unit={countdown.minutes} label='Minutes' />
+      <h2>:</h2>
+      <CountdownUnit unit={countdown.seconds} label='Seconds' />
+    </div>
+  );
+};
+
+Countdown.PropTypes = {
+  countdown: PropTypes.number
 };
 
 export default Countdown;
