67663d1f89e62d521484ac784602d6705c735fa0
Maria Shimkovska <mshimkov@stetson.edu>
2020-08-23 21:35:15 -0700

    Added conditional rendering to display submissions are closed when timer runs our

--- src/components/Countdown/CountdownContainer.js
+++ src/components/Countdown/CountdownContainer.js
@@ -10,33 +10,54 @@ const CountdownContainer = () => {
     seconds: 0,
   });
 
+  const [active, setActive] = React.useState(false);
+
   useEffect(() => {
     getRemainingTime();
-    setInterval(() => {
+
+    const interval = setInterval(() => {
       getRemainingTime();
+      console.log('hey')
+      if (getRemainingTime() < 0) {
+        setActive(false);
+        console.log('hi')
+        clearInterval(interval);
+      }
     }, 1000);
+
   }, []);
 
   const getRemainingTime = () => {
-    const countDownDate = new Date('Sept 1, 2020 23:59:59').getTime();
+    const countDownDate = new Date('September 1, 2020 23:59:59').getTime();
     const now = new Date().getTime();
-    const distance = countDownDate - now;
+    const difference = countDownDate - now;
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
     const hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
     );
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
     setCountdown({ days, hours, minutes, seconds });
+
+    if (difference >= 0 ) {
+      setActive(true);
+    }
+
+    return difference;
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.countdownContainer}>
-        <Countdown countdown={countdown} />
-      </div>
+      {
+        active
+          ? <div className={styles.countdownContainer}>
+            <Countdown countdown={countdown} />
+          </div>
+          :
+          <p className={styles.closedSubmittions}>Submissions are closed</p>
+      }
     </div>
   );
 };
