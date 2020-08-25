fffa8b14e7960792854aa28524dec639665e00ac
Jerel Miller <jerelmiller@gmail.com>
2020-08-25 12:08:16 -0700

    chore: update copy and organization of the developer events page

--- src/pages/developer-events.js
+++ src/pages/developer-events.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import cx from 'classnames';
 import SEO from '../components/Seo';
-import { Button, Video } from '@newrelic/gatsby-theme-newrelic';
+import { Button } from '@newrelic/gatsby-theme-newrelic';
+import { Link } from 'gatsby';
 import PageLayout from '../components/PageLayout';
-import ExternalLink from '../components/ExternalLink';
 import FeatherIcon from '../components/FeatherIcon';
 import nerdDays from '../images/nerd-days/nerd-days.png';
-import styles from './nerd-days.module.scss'
+import styles from './nerd-days.module.scss';
 
 const EventLandingPage = () => {
   return (
@@ -17,20 +17,26 @@ const EventLandingPage = () => {
         <PageLayout.Content>
           <section className={cx(styles.section, styles.twoColumn)}>
             <div>
-              <h3>
-                Developer Events <br />
-              </h3>
+              <p className="intro-text">
+                There are a lot of developer events out there, so we've curated
+                a list of events we're hosting or sponsoring that are dedicated
+                to developers, engineers, problem solvers, and builders like
+                you.
+              </p>
+              <h2>Featured event</h2>
+              <h3>Nerd Days 1.0: Return of the Data Nerd</h3>
               <p>
-                There are a lot of developer events out there, so we've curated a list of events we're hosting or sponsoring that are dedicated to developers, engineers, problem solvers, and builders like you.<br /><br />
-
-                <strong>UPCOMING EVENTS</strong><br />
-                Nerd Days 1.0: Return of the Data Nerd<br />
-                Nerd Days is a FREE engineering conference that kicks off October 13 (Dates vary by region). Focused on building more perfect software, our goal is to spend less time looking at slides that tell you what software can do and more time on getting your hands on the software to solve problems efficiently.
+                Nerd Days is a FREE engineering conference that kicks off
+                October 13 (Dates vary by region). Focused on building more
+                perfect software, our goal is to spend less time looking at
+                slides that tell you what software can do and more time on
+                getting your hands on the software to solve problems
+                efficiently.
               </p>
               <Button
-                as={ExternalLink}
+                as={Link}
                 variant={Button.VARIANT.PRIMARY}
-                href="https://developer.newrelic.com/nerd-days/"
+                href="/nerd-days"
               >
                 Register here
                 <FeatherIcon
@@ -40,26 +46,54 @@ const EventLandingPage = () => {
               </Button>
             </div>
             <img
+              src={nerdDays}
               className={styles.img}
               alt="Nerd Days 1.0: Return of the Data Nerd"
             />
           </section>
-          <section className={styles.section}>
           <h2>Upcoming Events</h2>
-          </section>
           <section className={cx(styles.section, styles.assetTable)}>
             <div className={styles.point}>
-              <h4>KubeCon and CloudNativeCon North America 2020</h4>
-              <p>November 17-20</p>
-              <p>The Cloud Native Computing Foundation’s flagship conference gathers adopters and technologists from leading open source and cloud native communities.</p>
+              <h3>Nerd Days 1.0: Return of the Data Nerd</h3>
+              <p>
+                <strong>US:</strong> October 13, 2020
+                <br />
+                <strong>EMEA:</strong> October 21, 2020
+                <br />
+                <strong>APJ:</strong> October 22, 2020
+                <br />
+              </p>
+              <p>
+                Nerd Days is a FREE engineering conference that kicks off
+                October 13 (Dates vary by region). Focused on building more
+                perfect software, our goal is to spend less time looking at
+                slides that tell you what software can do and more time on
+                getting your hands on the software to solve problems
+                efficiently.
+              </p>
             </div>
-            <h2>Past Events</h2>
           </section>
           <section className={cx(styles.section, styles.assetTable)}>
             <div className={styles.point}>
-              <h4>KubeCon and CloudNativeCon Europe 2020</h4>
+              <h3>KubeCon and CloudNativeCon North America 2020</h3>
+              <p>November 17-20</p>
+              <p>
+                The Cloud Native Computing Foundation’s flagship conference
+                gathers adopters and technologists from leading open source and
+                cloud native communities.
+              </p>
+            </div>
+          </section>
+          <h2>Past Events</h2>
+          <section className={cx(styles.section, styles.assetTable)}>
+            <div className={styles.point}>
+              <h3>KubeCon and CloudNativeCon Europe 2020</h3>
               <p>August 17 – 20</p>
-              <p>The Cloud Native Computing Foundation’s flagship conference gathers adopters and technologists from leading open source and cloud native communities.</p>
+              <p>
+                The Cloud Native Computing Foundation’s flagship conference
+                gathers adopters and technologists from leading open source and
+                cloud native communities.
+              </p>
             </div>
           </section>
         </PageLayout.Content>
