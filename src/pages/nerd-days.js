491d74c8359ba44c189cdeaa9ecf7e7c35f2fa2b
Daniel Golden <danielgolden90@gmail.com>
2020-09-01 14:56:56 -0400

    feat: add in markup and styles for nerd days - Markup and styles for hero and speakers sections

--- src/pages/nerd-days.js
+++ src/pages/nerd-days.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'gatsby';
 import cx from 'classnames';
 import PageLayout from '../components/PageLayout';
-import MarketoForm from '../components/MarketoForm';
+import { Button, ExternalLink } from '@newrelic/gatsby-theme-newrelic';
 import SEO from '../components/Seo';
-import nerdDays from '../images/nerd-days/nerd-days.png';
+import nrLogo from '../images/nerd-days/nr-logo.svg';
 import styles from './nerd-days.module.scss';
 
 const NerdDaysPage = () => {
@@ -12,59 +12,102 @@ const NerdDaysPage = () => {
     <>
       <SEO />
       <PageLayout type={PageLayout.TYPE.SINGLE_COLUMN}>
-        <PageLayout.Header title="New Relic Nerd Days" />
         <PageLayout.Content>
-          <section className={cx(styles.section, styles.twoColumnAlt)}>
-            <div>
-              <img
-                className={styles.img}
-                src={nerdDays}
-                alt="nerd days header"
-              />
-              <p>
-                Nerd Days is a <b>FREE</b> engineering conference that kicks off
-                October 13 <em>(Dates vary by region)</em>. Focused on building
-                more perfect software, our goal is to spend less time looking at
-                slides that tell you what software can do and more time on
-                getting your hands on the software to solve problems
-                efficiently.
-              </p>
-              <p>
-                You’ll hear from fellow engineers who built New Relic solutions
-                and New Relic users from various industries. Whether you’re new
-                or a data nerd, there’s an interactive session for you.
-              </p>
-              <p>We look forward to building with you during Nerd Days! </p>
-              <h2 className={styles.h2}>CALL FOR PROPOSALS</h2>
-              <p>
-                Got an interesting topic you want to share with your fellow
-                engineers?{' '}
-                <Link to="https://docs.google.com/forms/d/e/1FAIpQLSe8LOilCrD_TCUPyHFHG_QzVW2UdUR0UKZ8H8WNylz0flB7OQ/viewform">
-                  Submit your proposals
-                </Link>{' '}
-                by September 1, 2020 at 11:59 PM PT. For more details, check out
-                the one-pager. Accepted proposals will be contacted by September
-                30, 2020 at the latest.
-              </p>
-              <p>
-                For an overview and tips for the call for proposals process,
-                <a
-                  href="https://resources.newrelic.com/nerd-days-call-for-proposals"
-                  alt="cfp guide"
-                >
-                  {' '}
-                  check out our guide.
-                </a>
-              </p>
+          <section className={cx(styles.heroSection)}>
+            <div className={cx(styles.heroLogoContainer)}>
+              <img src={nrLogo} className={cx(styles.heroNrLogo)}/>
+              <h1 className={cx(styles.heroLogo)}>nerd days 1.0</h1>
             </div>
-            <MarketoForm
-              id={3525}
-              title="REGISTER FOR NERD DAYS | AMERICAS"
-              munchkinId="412-MZS-894"
-              publishableKey="pk_4d10daa544de6f993a9a9ce002ccd1c6"
-            />
+            <h3 className={cx(styles.heroHeading)}>
+              Nerd Days is a <em className={cx(styles.heroHeadingHighlight)}>free</em>{` `} 
+              1-day event focused on building more perfect software
+            </h3>
+            <p className={cx(styles.heroDescription)}>
+              Our goal is to spend less time looking at slides that tell you what software 
+              can do and more time on getting your hands on the software to solve problems 
+              efficiently. Morbi malesuada nulla nec purus convallis consequat. Vivamus id 
+              mollis quam. Morbi ac comm. You’ll hear from fellow engineers who built New 
+              Relic solutions and New Relic users from various industries. Whether you’re 
+              new or a data nerd, there’s an interactive session for you. We look forward 
+              to building with you during Nerd Days.
+            </p>
+            <ul className={cx(styles.heroCtaContainer)}>
+              <li className={cx(styles.heroCtaContainerItem)}>
+                <Button
+                  as={ExternalLink}
+                  variant={Button.VARIANT.PRIMARY}
+                  className={cx(styles.ctaButton)}
+                >
+                  US Registration
+                </Button>
+                <span className={cx(styles.registrationButtonDate)}>Oct 13, 2020</span>
+              </li>
+              <li className={cx(styles.heroCtaContainerItem)}>
+                <Button
+                  as={ExternalLink}
+                  variant={Button.VARIANT.PRIMARY}
+                  className={cx(styles.ctaButton)}
+                >
+                  EMEA Registration
+                </Button>
+                <span className={cx(styles.registrationButtonDate)}>Oct 20, 2020</span>
+              </li>
+              <li className={cx(styles.heroCtaContainerItem)}>
+                <Button
+                  as={ExternalLink}
+                  variant={Button.VARIANT.PRIMARY}
+                  className={cx(styles.ctaButton)}
+                >
+                  APJ Registration
+                </Button>
+                <span className={cx(styles.registrationButtonDate)}>Oct 22, 2020</span>
+              </li>
+            </ul>
           </section>
-          <section />
+
+          <section className={cx(styles.speakersSection, styles.eventSection)}>
+            <h3 className={cx(styles.sectionHeading)}>Speaker line up</h3>
+            <p className={cx(styles.sectionDescription)}>Nulla vitae elit libero, a pharetra augue nullam id dolor id nibh ultricies vehicula ut id elit</p>
+
+            <ul className={cx(styles.speakerList)}>
+              <li className={cx(styles.speakerListItem)}>
+                <div className={cx(styles.speakerListItemPhoto)}>
+                  <img src="https://dummyimage.com/256x194/#444/000333.jpg" alt="Photo of..."/>
+                </div>
+                <div className={cx(styles.speakerListItemCopy)}>
+                  <h4 className={cx(styles.speakerListItemHeading)}>Dylan Hernandez</h4>
+                  <p className={cx(styles.speakerListItemDescription)}>Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem lacinia quam.</p>
+                </div>
+              </li>
+              <li className={cx(styles.speakerListItem)}>
+                <div className={cx(styles.speakerListItemPhoto)}>
+                  <img src="https://dummyimage.com/256x194/#444/000333.jpg" alt="Photo of..."/>
+                </div>
+                <div className={cx(styles.speakerListItemCopy)}>
+                  <h4 className={cx(styles.speakerListItemHeading)}>Alisha Edwards</h4>
+                  <p className={cx(styles.speakerListItemDescription)}>Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem lacinia quam.</p>
+                </div>
+              </li>
+              <li className={cx(styles.speakerListItem)}>
+                <div className={cx(styles.speakerListItemPhoto)}>
+                  <img src="https://dummyimage.com/256x194/#444/000333.jpg" alt="Photo of..."/>
+                </div>
+                <div className={cx(styles.speakerListItemCopy)}>
+                  <h4 className={cx(styles.speakerListItemHeading)}>Ralph McGibbons</h4>
+                  <p className={cx(styles.speakerListItemDescription)}>Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem lacinia quam.</p>
+                </div>
+              </li>
+              <li className={cx(styles.speakerListItem)}>
+                <div className={cx(styles.speakerListItemPhoto)}>
+                  <img src="https://dummyimage.com/256x194/#444/000333.jpg" alt="Photo of..."/>
+                </div>
+                <div className={cx(styles.speakerListItemCopy)}>
+                  <h4 className={cx(styles.speakerListItemHeading)}>Anita Baker</h4>
+                  <p className={cx(styles.speakerListItemDescription)}>Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem lacinia quam.</p>
+                </div>
+              </li>
+            </ul>
+          </section>
         </PageLayout.Content>
       </PageLayout>
     </>
