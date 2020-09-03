87548fb95c86a7e095cf0991d766325518ddcba4
Jerel Miller <jerelmiller@gmail.com>
2020-09-03 00:47:16 -0700

    chore: handle objects with no examples

--- plugins/gatsby-source-newrelic-sdk/src/exampleInfo.js
+++ plugins/gatsby-source-newrelic-sdk/src/exampleInfo.js
@@ -1,12 +1,13 @@
 const IGNORED_PREVIEWS = ['Dropdown', 'Modal', 'Tooltip'];
 
 exports.getExamples = (name, obj) => {
-  return (obj[name].__docs__.tags.examples || []).map(
-    ({ label, options, sourceCode }) => ({
-      label,
-      sourceCode,
-      live: IGNORED_PREVIEWS.includes(name) ? false : options.live,
-      preview: IGNORED_PREVIEWS.includes(name) ? false : options.live,
-    })
-  );
+  const docs = obj[name].__docs__ || {};
+  const tags = docs.tags || {};
+
+  return (tags.examples || []).map(({ label, options, sourceCode }) => ({
+    label,
+    sourceCode,
+    live: IGNORED_PREVIEWS.includes(name) ? false : options.live,
+    preview: IGNORED_PREVIEWS.includes(name) ? false : options.live,
+  }));
 };
