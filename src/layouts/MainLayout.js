ad1c1538946956fc4341aca3b9a15cf955560f8d
Zack Stickles <zstickles@gmail.com>
2020-09-08 14:58:14 -0700

    chore: clean up imports

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 
