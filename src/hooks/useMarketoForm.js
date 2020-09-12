6f64464936de77108aa79f2c253cd020dc2ac594
Jemiah Sius <jsius@newrelic.com>
2020-09-11 20:21:28 -0400

    switch to absolute path

--- src/hooks/useMarketoForm.js
+++ src/hooks/useMarketoForm.js
@@ -9,7 +9,7 @@ const useMarketoForm = (munchkinId, id, publishableKey) => {
       (form) => {
         // eslint-disable-next-line no-unused-vars
         form.onSuccess(function (values, followUpUrl) {
-          location.href = '/nerd-days-thank-you';
+          location.href = 'https://developer.newrelic.com/nerd-days-thank-you';
           return false;
         });
       }
