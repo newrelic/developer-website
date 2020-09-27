b1cb60c6645d09c871c9b0368352cb9590936091
Jerel Miller <jerelmiller@gmail.com>
2020-09-26 22:12:54 -0700

    refactor: use partial application to make fetching bundle easier

--- plugins/gatsby-source-newrelic-sdk/src/loadSdk.js
+++ plugins/gatsby-source-newrelic-sdk/src/loadSdk.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const BASE_URL =
   'https://hypertext-sandbox.nr-assets.net/wanda--wanda-ec-ui--nr1-docs';
 
-const getBundle = async (src, { cache }) => {
+const getBundle = (cache) => async (src) => {
   const script = await cache.get(src);
 
   if (script) {
@@ -19,21 +19,18 @@ const getBundle = async (src, { cache }) => {
 module.exports = async (release, { cache }) => {
   require('./mockGlobals');
 
+  const bundle = getBundle(cache);
+
   const bundles = await Promise.all([
-    getBundle('https://nr1.nr-assets.net/lib/d3/3.5.17/d3.js', { cache }),
-    getBundle(
-      'https://nr1.nr-assets.net/lib/react/16.6.3/react.development.js',
-      { cache }
+    bundle('https://nr1.nr-assets.net/lib/d3/3.5.17/d3.js'),
+    bundle('https://nr1.nr-assets.net/lib/react/16.6.3/react.development.js'),
+    bundle(
+      'https://nr1.nr-assets.net/lib/react/16.6.3/react-dom.development.js'
     ),
-    getBundle(
-      'https://nr1.nr-assets.net/lib/react/16.6.3/react-dom.development.js',
-      { cache }
+    bundle(
+      'https://nr1.nr-assets.net/lib/react-router-dom/4.2.2/react-router-dom.js'
     ),
-    getBundle(
-      'https://nr1.nr-assets.net/lib/react-router-dom/4.2.2/react-router-dom.js',
-      { cache }
-    ),
-    getBundle(`${BASE_URL}-${release}.js`, { cache }),
+    bundle(`${BASE_URL}-${release}.js`),
   ]);
 
   return vm.runInThisContext(
