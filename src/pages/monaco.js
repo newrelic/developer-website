ae436c4c955128f73d1fe160e7b10837f905dc82
Cayla Hamann <chamann@newrelic.com>
2020-10-08 15:01:37 -0400

    feat: hover provider works

--- src/pages/monaco.js
+++ src/pages/monaco.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash');
+
 const monacoConfig = (monaco, sdk) => {
   monaco.languages.registerCompletionItemProvider('javascript', {
     provideCompletionItems: (model, position) => {
@@ -13,6 +15,24 @@ const monacoConfig = (monaco, sdk) => {
       };
     },
   });
+  monaco.languages.registerHoverProvider('javascript', {
+    provideHover: (model, position, token) => {
+      const source = new monaco.CancellationTokenSource(token);
+      source.cancel();
+      if (!model.getWordAtPosition(position)) return null;
+      const word = model.getWordAtPosition(position).word;
+      const range = {
+        startLineNumber: position.lineNumber,
+        endLineNumber: position.lineNumber,
+        startColumn: word.startColumn,
+        endColumn: word.endColumn,
+      };
+      if (Object.keys(sdk).includes(word)) {
+        return createHoverProvider(range, sdk, word);
+      }
+      return null;
+    },
+  });
 };
 
 const createComponentCompletion = (range, monaco, sdk) => {
@@ -25,6 +45,22 @@ const createComponentCompletion = (range, monaco, sdk) => {
       range: range,
     };
   });
+};
+
+const createHoverProvider = (range, sdk, component) => {
+  return {
+    range: range,
+    contents: [
+      {
+        value:
+          `### ${component} Example Implementation \n \`\`\`javascript\n${sdk[component]?.__docs__?.tags.examples[0].sourceCode}\n\`\`\`` ||
+          '',
+      },
+      {
+        value: `[Learn more here](https://developer.newrelic.com/components/${component.toLowerCase()})`,
+      },
+    ],
+  };
 };
 
 export default monacoConfig;
