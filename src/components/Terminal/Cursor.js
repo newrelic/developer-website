ecbf2613f7c74fdf3597a55d9366e8c817322c81
Jerel Miller <jerelmiller@gmail.com>
2020-10-14 16:43:53 -0700

    chore: create a cursor

---
+++ src/components/Terminal/Cursor.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { css, keyframes } from '@emotion/core';
+
+const blink = keyframes`
+  0%, 49% {
+    background: none;
+  }
+
+  50%, 100% {
+    background: #fafafa;
+  }
+`;
+
+const Cursor = () => (
+  <span
+    css={css`
+      display: inline-block;
+      width: 1ch;
+      height: 1.25em;
+      animation: ${blink} 1s infinite;
+    `}
+  />
+);
+
+export default Cursor;
