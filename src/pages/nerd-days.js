742cc0fa0c812f3d7bb91d3fdcb6a4ba93919187
Jemiah Sius <jsius@newrelic.com>
2020-10-15 09:40:52 -0400

    remove AMER registration

--- src/pages/nerd-days.js
+++ src/pages/nerd-days.js
@@ -233,13 +233,16 @@ const NerdDaysPage = () => {
                   have any questions about Nerd Days please emails{' '}
                   <a href="mailto:deco@newrelic.com">deco@newrelic.com</a>.
                 </p>
-                <h3
-                  css={css`
-                    margin: 25px 0 0 0;
-                  `}
-                >
-                  Additional Nerd Days Events
-                </h3>
+              </div>
+              {/* <MarketoForm
+                id={4783}
+                title="REGISTER FOR NERD DAYS | AMERICAS"
+                munchkinId="412-MZS-894"
+                publishableKey="pk_4d10daa544de6f993a9a9ce002ccd1c6"
+                redirectLink="/nerd-days-thank-you"
+              /> */}
+              <div>
+                <h3>Upcoming Nerd Days</h3>
                 <ul
                   css={css`
                     padding: 0;
@@ -262,13 +265,6 @@ const NerdDaysPage = () => {
                   </CtaItem>
                 </ul>
               </div>
-              <MarketoForm
-                id={4783}
-                title="REGISTER FOR NERD DAYS | AMERICAS"
-                munchkinId="412-MZS-894"
-                publishableKey="pk_4d10daa544de6f993a9a9ce002ccd1c6"
-                redirectLink="/nerd-days-thank-you"
-              />
             </div>
           </Section>
           <Section className={cx(styles.speakersSection)}>
@@ -916,7 +912,7 @@ const CtaItem = ({ date, to, children }) => (
   <li
     css={css`
       margin-top: 0.5rem;
-      margin-right: 1.25rem;
+      margin-right: .5rem;
       display: flex;
       flex-direction: column;
       justify-content: center;
