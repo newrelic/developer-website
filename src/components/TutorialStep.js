271969ba8eb12d15195e4266bbb500d2bd0596c2
Jerel Miller <jerelmiller@gmail.com>
2020-10-16 01:01:05 -0700

    chore: show code blocks in right column

--- src/components/TutorialStep.js
+++ src/components/TutorialStep.js
@@ -2,6 +2,27 @@ import React, { Children, cloneElement } from 'react';
 import PropTypes from 'prop-types';
 import { css, ClassNames } from '@emotion/core';
 import { isMdxType } from '../utils/mdx';
+import { isShellCommand, isCodeBlock } from '../utils/codeBlock';
+
+const isCodeLikeBlock = (child) =>
+  isShellCommand(child) ||
+  isCodeBlock(child) ||
+  isMdxType(child, 'TutorialEditor');
+
+const findIndexes = (arr, predicate) =>
+  arr.reduce(
+    (memo, item, idx) => (predicate(item) ? [...memo, idx] : memo),
+    []
+  );
+
+const sliceRegions = (indices) =>
+  indices.reduce((memo, boundary, idx) => {
+    if (idx === 0) {
+      return [[0, boundary]];
+    }
+
+    return [...memo, [indices[idx - 1], boundary]];
+  }, []);
 
 const TutorialStep = ({ children, stepNumber, totalSteps }) => {
   children = Children.toArray(children);
@@ -10,6 +31,19 @@ const TutorialStep = ({ children, stepNumber, totalSteps }) => {
   const content = title
     ? children.filter((child) => child !== title)
     : children;
+
+  const codeBlockIndices = findIndexes(content, isCodeLikeBlock);
+
+  const columns = codeBlockIndices
+    .reduce((memo, boundary, idx) => {
+      const previousIdx = idx === 0 ? 0 : codeBlockIndices[idx - 1] + 1;
+
+      return [
+        ...memo,
+        [content.slice(previousIdx, boundary), content[boundary]],
+      ];
+    }, [])
+    .concat(content.slice(codeBlockIndices[codeBlockIndices.length - 1] + 1));
 
   return (
     <div
@@ -36,7 +70,29 @@ const TutorialStep = ({ children, stepNumber, totalSteps }) => {
           }
         </ClassNames>
       )}
-      <div>{content}</div>
+      {codeBlockIndices.length === 0
+        ? content
+        : columns.map((column, idx) => {
+            return Array.isArray(column) ? (
+              <div
+                key={idx}
+                css={css`
+                  display: grid;
+                  grid-template-columns: repeat(2, calc(50% - 0.5rem));
+                  grid-gap: 1rem;
+
+                  &:not(:last-child) {
+                    margin-bottom: 1rem;
+                  }
+                `}
+              >
+                <div>{column[0]}</div>
+                <div>{column[1]}</div>
+              </div>
+            ) : (
+              column
+            );
+          })}
     </div>
   );
 };
