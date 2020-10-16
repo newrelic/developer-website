3beeeb85b63cbafdb5dad79a64c4c9ac9599c5fd
Jerel Miller <jerelmiller@gmail.com>
2020-10-15 17:51:16 -0700

    refactor: replace deeply nested code blocks with tutorial editor

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -1,4 +1,4 @@
-import React, { Children, cloneElement } from 'react';
+import React, { Children, cloneElement, isValidElement } from 'react';
 import PropTypes from 'prop-types';
 import { isCodeBlock, isShellCommand, hasFileName } from '../utils/codeBlock';
 import { isMdxType } from '../utils/mdx';
@@ -10,12 +10,9 @@ const Tutorial = ({ children }) => {
   children = Children.toArray(children);
 
   useOnMount(() => {
-    validateChildren(children);
+    validate(children);
   });
 
-  const isSectioned = children.some((child) =>
-    isMdxType(child, 'TutorialSection')
-  );
   const projectElement = isMdxType(children[0], 'Project') ? children[0] : null;
 
   if (projectElement) {
@@ -26,104 +23,17 @@ const Tutorial = ({ children }) => {
     ? parseProjectStateFromConfig(projectElement)
     : parseProjectStateFromChildren(children);
 
-  const [elements] = isSectioned
-    ? updateCodeBlockInSections(children, initialProjectState)
-    : updateCodeBlocksInSteps(children, initialProjectState);
-
-  return elements;
+  return updateCodeBlocks(children, initialProjectState);
 };
 
 Tutorial.propTypes = {
   children: PropTypes.node,
 };
 
-const updateCodeBlockInSections = (children, initialProjectState) => {
-  return children.reduce(
-    ([children, currentProjectState], child) => {
-      if (!isMdxType(child, 'TutorialSection')) {
-        return [[...children, child], currentProjectState];
-      }
-
-      const [steps, projectState] = updateCodeBlocksInSteps(
-        child.props.children,
-        currentProjectState
-      );
-
-      return [
-        [...children, cloneElement(child, { children: steps })],
-        projectState,
-      ];
-    },
-    [[], initialProjectState]
-  );
-};
-
-const updateCodeBlocksInSteps = (children, initialProjectState) => {
-  return Children.toArray(children).reduce(
-    ([steps, currentProjectState], stepElement, idx) => {
-      const [children, projectState] = swapCodeBlocks(
-        stepElement.props.children,
-        currentProjectState
-      );
-
-      return [
-        [
-          ...steps,
-          cloneElement(stepElement, {
-            children,
-            stepNumber: idx + 1,
-            totalSteps: children.length,
-          }),
-        ],
-        projectState,
-      ];
-    },
-    [[], initialProjectState]
-  );
-};
+const isProjectCodeBlock = (child) =>
+  isCodeBlock(child) && !isShellCommand(child) && hasFileName(child);
 
 const clone = (map) => new Map(map);
-
-const swapCodeBlocks = (children, initialProjectState) => {
-  return Children.toArray(children).reduce(
-    ([children, currentProjectState], child, idx) => {
-      if (!isCodeBlock(child) || isShellCommand(child)) {
-        return [[...children, child], currentProjectState];
-      }
-
-      const props = extractCodeBlockProps(child);
-
-      if (!currentProjectState.has(props.fileName)) {
-        throw new Error(`The following block does not have a file name that matches the project. Please ensure the code block has a \`fileName\` specified:
-
-\`\`\`${props.language}
-${props.code}
-\`\`\`
-`);
-      }
-
-      const { code: prevCode } = currentProjectState.get(props.fileName);
-      const projectState = clone(currentProjectState).set(props.fileName, {
-        code: props.code,
-        language: props.language,
-      });
-
-      return [
-        [
-          ...children,
-          <TutorialEditor
-            key={idx}
-            focusedFileName={props.fileName}
-            diff={diffLines(prevCode, props.code)}
-            project={projectState}
-          />,
-        ],
-        projectState,
-      ];
-    },
-    [[], initialProjectState]
-  );
-};
 
 const parseProjectStateFromConfig = (configElement) => {
   return Children.toArray(configElement.props.children)
@@ -138,18 +48,13 @@ const parseProjectStateFromConfig = (configElement) => {
 const parseProjectStateFromChildren = (children) => {
   const project = new Map();
 
-  visit(
-    children,
-    (child) =>
-      isCodeBlock(child) && !isShellCommand(child) && hasFileName(child),
-    (codeBlock) => {
-      const { fileName, ...props } = extractCodeBlockProps(codeBlock);
+  visit(children, isProjectCodeBlock, (codeBlock) => {
+    const { fileName, ...props } = extractCodeBlockProps(codeBlock);
 
-      if (!project.has(fileName)) {
-        project.set(fileName, { ...props, code: '' });
-      }
+    if (!project.has(fileName)) {
+      project.set(fileName, { ...props, code: '' });
     }
-  );
+  });
 
   return project;
 };
@@ -164,11 +69,52 @@ const visit = (children, guard, fn, parent = null) => {
   });
 };
 
-const validateChildren = (children) => {
-  const isSectioned = children.some((child) =>
-    isMdxType(child, 'TutorialSection')
-  );
+const reduceChildren = (children, select, reducer, parent = null) => {
+  if (typeof children === 'string') {
+    return children;
+  }
 
+  return Children.map(children, (child, idx) => {
+    if (select(child, idx, parent)) {
+      return reducer(child, idx, parent);
+    }
+
+    if (child.props?.children) {
+      return cloneElement(child, {
+        children: reduceChildren(child.props.children, select, reducer, child),
+      });
+    }
+
+    return child;
+  });
+};
+
+const updateCodeBlocks = (children, project) => {
+  return reduceChildren(children, isProjectCodeBlock, (codeBlock) => {
+    const { fileName, code, ...props } = extractCodeBlockProps(codeBlock);
+
+    if (!project.has(fileName)) {
+      return codeBlock;
+    }
+
+    const { code: prevCode } = project.get(fileName);
+
+    project = clone(project).set(fileName, {
+      ...props,
+      code,
+    });
+
+    return (
+      <TutorialEditor
+        focusedFileName={fileName}
+        diff={diffLines(prevCode, code)}
+        project={project}
+      />
+    );
+  });
+};
+
+const validate = (children) => {
   const projectElementIdx = children.findIndex((child) =>
     isMdxType(child, 'Project')
   );
@@ -176,29 +122,6 @@ const validateChildren = (children) => {
   if (projectElementIdx > 0) {
     throw new Error(
       'Tutorial: A `Project` element was detected but does not reside as the first child of the `Tutorial`. Please move it to the first child of the `Tutorial`.'
-    );
-  }
-
-  children = children.slice(1);
-  isSectioned ? validateSections(children) : validateTutorialSteps(children);
-};
-
-const validateSections = (children) => {
-  const isValid = children.every((child) => !isMdxType(child, 'TutorialStep'));
-
-  if (!isValid) {
-    throw new Error(
-      'Tutorial: A `TutorialStep` was detected outside of a `TutorialSection`. Please ensure the element is wrapped in a `TutorialSection`.'
-    );
-  }
-};
-
-const validateTutorialSteps = (children) => {
-  const isValid = children.every((child) => isMdxType(child, 'TutorialStep'));
-
-  if (!isValid) {
-    throw new Error(
-      'Tutorial: Every child of a `Tutorial` must be wrapped in a `TutorialStep`. If you meant to include a non `TutorialStep` in the `Tutorial`, wrap each section in a `TutorialSection` or move the content inside of the `TutorialStep`.'
     );
   }
 };
