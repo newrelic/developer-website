ee58f4641cb020339571fd29a6628c46c54c3ed0
Jerel Miller <jerelmiller@gmail.com>
2020-10-15 18:11:39 -0700

    fix: add back step counter within a tutorial section

--- src/components/TutorialSection.js
+++ src/components/TutorialSection.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Children, cloneElement } from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
+import { isMdxType } from '../utils/mdx';
 
 const TutorialSection = ({ children }) => {
+  const totalSteps = Children.toArray(children).filter((child) =>
+    isMdxType(child, 'TutorialStep')
+  ).length;
+  let step = 1;
+
   return (
     <section
       css={css`
@@ -13,7 +19,13 @@ const TutorialSection = ({ children }) => {
         }
       `}
     >
-      {children}
+      {Children.map(children, (child) => {
+        if (isMdxType(child, 'TutorialStep')) {
+          return cloneElement(child, { stepNumber: step++, totalSteps });
+        }
+
+        return child;
+      })}
     </section>
   );
 };
