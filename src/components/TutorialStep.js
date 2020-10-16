49c9370bf8861e851e5de739f5b4e8610f4af41c
Jerel Miller <jerelmiller@gmail.com>
2020-10-15 20:59:25 -0700

    chore: use cloneElement on title

--- src/components/TutorialStep.js
+++ src/components/TutorialStep.js
@@ -1,11 +1,15 @@
-import React, { Children } from 'react';
+import React, { Children, cloneElement } from 'react';
 import PropTypes from 'prop-types';
-import { css } from '@emotion/core';
+import { css, ClassNames } from '@emotion/core';
 import { isMdxType } from '../utils/mdx';
+import { isCodeBlock } from '../utils/codeBlock';
 
 const TutorialStep = ({ children, stepNumber, totalSteps }) => {
   children = Children.toArray(children);
   const title = isMdxType(children[0], 'h3') ? children[0] : null;
+  const content = title
+    ? children.filter((child) => child !== title)
+    : children;
 
   return (
     <div
@@ -24,18 +28,19 @@ const TutorialStep = ({ children, stepNumber, totalSteps }) => {
         Step {stepNumber} of {totalSteps}
       </p>
       {title && (
-        <h3
-          {...title.props}
-          css={css`
-            font-size: 1rem;
-            font-weight: bold;
-            margin-top: 0 !important;
-          `}
-        >
-          {title.props.children}
-        </h3>
+        <ClassNames>
+          {({ css }) =>
+            cloneElement(title, {
+              className: css`
+                font-size: 1rem;
+                font-weight: bold;
+                margin-top: 0 !important;
+              `,
+            })
+          }
+        </ClassNames>
       )}
-      {children.filter((child) => child !== title)}
+      <div>{content}</div>
     </div>
   );
 };
