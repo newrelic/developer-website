34156d443e3f75a6ea67a7846a553bb5bf8a8e74
Clinton <clinton.langosch@gmail.com>
2020-10-16 09:50:14 -0700

    feat: add X-Content-Type-Options request header

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,6 +3,7 @@ import fetch from 'isomorphic-fetch';
 
 const headers = {
   'Content-Type': 'application/json',
+  'X-Content-Type-Options': 'nosniff',
   'X-API-Key': 'developer.newrelic.com',
   // We need this header or the 'accept: application/json' to be set for
   // SG to treat this as a programatic request and provide a 401 instead
