bedc1f4aca0df3cc5bf55ea8cba7322fceccc91b
Jerel Miller <jerelmiller@gmail.com>
2020-10-29 23:22:21 -0700

    chore: dont load older react/react-dom. Defer/async other deps

--- gatsby-node.js
+++ gatsby-node.js
@@ -134,5 +134,23 @@ exports.onCreateWebpackConfig = ({ actions, plugins }) => {
     externals: {
       tessen: 'Tessen',
     },
+    module: {
+      rules: [
+        {
+          test: require.resolve('react'),
+          loader: 'expose-loader',
+          options: {
+            exposes: ['React'],
+          },
+        },
+        {
+          test: require.resolve('react-dom'),
+          loader: 'expose-loader',
+          options: {
+            exposes: ['ReactDOM'],
+          },
+        },
+      ],
+    },
   });
 };
--- plugins/gatsby-source-newrelic-sdk/gatsby-ssr.js
+++ plugins/gatsby-source-newrelic-sdk/gatsby-ssr.js
@@ -2,48 +2,33 @@ import React from 'react';
 import { BASE_URL } from './src/constants';
 
 export const onPreRenderHTML = (
-  {
-    getHeadComponents,
-    replaceHeadComponents,
-    getPostBodyComponents,
-    replacePostBodyComponents,
-  },
+  { getPostBodyComponents, replacePostBodyComponents },
   pluginOptions
 ) => {
   const { release } = pluginOptions;
 
-  replaceHeadComponents([
-    ...getHeadComponents(),
+  replacePostBodyComponents([
+    ...getPostBodyComponents(),
     <script
-      key="react"
-      crossOrigin="anonymous"
-      src="//nr1.nr-assets.net/lib/react/16.6.3/react.development.js"
-    />,
-    <script
-      key="react-dom"
-      crossOrigin="anonymous"
-      src="//nr1.nr-assets.net/lib/react/16.6.3/react-dom.development.js"
-    />,
-    <script
+      async
       key="react-router"
       crossOrigin="anonymous"
       src="//nr1.nr-assets.net/lib/react-router/4.2.0/react-router.js"
     />,
     <script
+      async
       key="react-router-dom"
       crossOrigin="anonymous"
       src="//nr1.nr-assets.net/lib/react-router-dom/4.2.2/react-router-dom.js"
     />,
     <script
+      async
       key="d3"
       crossOrigin="anonymous"
       src="//nr1.nr-assets.net/lib/d3/3.5.17/d3.js"
     />,
-  ]);
-
-  replacePostBodyComponents([
-    ...getPostBodyComponents(),
     <script
+      defer
       key="nr1-sdk"
       crossOrigin="anonymous"
       src={`${BASE_URL}-${release}.js`}
