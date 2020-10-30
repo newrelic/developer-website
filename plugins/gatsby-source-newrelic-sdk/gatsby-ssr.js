e5f54697760b2eb4758554cad392b5d9e1fe674e
Jerel Miller <jerelmiller@gmail.com>
2020-10-30 00:05:18 -0700

    chore: get rid of react router dep

--- plugins/gatsby-source-newrelic-sdk/gatsby-ssr.js
+++ plugins/gatsby-source-newrelic-sdk/gatsby-ssr.js
@@ -2,31 +2,28 @@ import React from 'react';
 import { BASE_URL } from './src/constants';
 
 export const onPreRenderHTML = (
-  { getPostBodyComponents, replacePostBodyComponents },
+  {
+    getHeadComponents,
+    replaceHeadComponents,
+    getPostBodyComponents,
+    replacePostBodyComponents,
+  },
   pluginOptions
 ) => {
   const { release } = pluginOptions;
 
-  replacePostBodyComponents([
-    ...getPostBodyComponents(),
-    <script
-      async
-      key="react-router"
-      crossOrigin="anonymous"
-      src="//nr1.nr-assets.net/lib/react-router/4.2.0/react-router.js"
-    />,
-    <script
-      async
-      key="react-router-dom"
-      crossOrigin="anonymous"
-      src="//nr1.nr-assets.net/lib/react-router-dom/4.2.2/react-router-dom.js"
-    />,
+  replaceHeadComponents([
+    ...getHeadComponents(),
     <script
       async
       key="d3"
       crossOrigin="anonymous"
       src="//nr1.nr-assets.net/lib/d3/3.5.17/d3.js"
     />,
+  ]);
+
+  replacePostBodyComponents([
+    ...getPostBodyComponents(),
     <script
       defer
       key="nr1-sdk"
