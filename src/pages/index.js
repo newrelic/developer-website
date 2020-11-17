a1efacb855dbfb4248d73154a58c0796aed49097
veextee <vi.tranx@gmail.com>
2020-11-17 14:42:22 -0800

    [update] png to jpg
    update image to correct file name in import

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ import styles from './index.module.scss';
 import devChampionBadge from '../images/developer-champion/dev-champion-badge.png';
 import podcastBadge from '../images/podcasts/podcasts-badge.png';
 import Countdown from '../components/Countdown';
-import header from '../images/header.png';
+import header from '../images/header.jpg';
 
 const getStartedGuides = [
   {
