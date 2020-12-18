cef4b054e4c7177fb2963e9044967d7c4359e228
Jerel Miller <jerelmiller@gmail.com>
2020-12-18 00:40:15 -0800

    feat: add a NewRelicSVG component

---
+++ src/components/NewRelicSVG.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
+
+const NewRelicSVG = ({ children, className, size }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className={className}
+    css={css`
+      fill: none;
+      stroke: currentColor;
+      stroke-width: 1.5;
+      stroke-linecap: round;
+      stroke-linejoin: round;
+      width: ${size};
+      height: ${size};
+    `}
+  >
+    {children}
+  </svg>
+);
+
+NewRelicSVG.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  size: PropTypes.string,
+};
+
+NewRelicSVG.defaultProps = {
+  size: '1em',
+};
+
+export default NewRelicSVG;
