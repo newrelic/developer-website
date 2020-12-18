fefa2176c41a6ca9ea9659d68fecddd65952eb9f
Jerel Miller <jerelmiller@gmail.com>
2020-12-18 00:40:29 -0800

    feat: add custom newrelic automation icon

---
+++ src/@newrelic/gatsby-theme-newrelic/icons/newrelic.js
@@ -0,0 +1,7 @@
+import defaultIcons from '@newrelic/gatsby-theme-newrelic/src/icons/newrelic';
+import automation from './newrelic/automation';
+
+export default {
+  ...defaultIcons,
+  automation,
+};
