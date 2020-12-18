070023b21c99f42d52c61e97b767283ad0cc0ab8
Jerel Miller <jerelmiller@gmail.com>
2020-12-18 00:51:29 -0800

    feat: add additional newrelic icons to the nr icons set that is used by the theme

--- src/@newrelic/gatsby-theme-newrelic/icons/newrelic.js
+++ src/@newrelic/gatsby-theme-newrelic/icons/newrelic.js
@@ -1,7 +1,19 @@
 import defaultIcons from '@newrelic/gatsby-theme-newrelic/src/icons/newrelic';
+import book from './newrelic/book';
+import buildApps from './newrelic/build-apps';
 import automation from './newrelic/automation';
+import developerChampions from './newrelic/developer-champions';
+import headphones from './newrelic/headphones';
+import location from './newrelic/location';
+import share from './newrelic/share';
 
 export default {
   ...defaultIcons,
   automation,
+  book,
+  'build-apps': buildApps,
+  'developer-champions': developerChampions,
+  headphones,
+  location,
+  share,
 };
