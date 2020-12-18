81c900eab1a739237bd7334591b4593beb7a5123
Jerel Miller <jerelmiller@gmail.com>
2020-12-18 00:27:04 -0800

    chore: fix search logic for pages

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,15 +6,19 @@ import pages from '../data/sidenav.json';
 import styles from './Navigation.module.scss';
 
 const filterPages = (pages, regex) => {
-  return pages.filter((page) => {
-    if (regex.test(page.title)) {
-      return true;
-    } else if (page.pages) {
-      return filterPages(page.pages, regex);
-    }
+  return pages
+    .map((page) => {
+      const filteredPages = filterPages(page.pages || [], regex);
 
-    return false;
-  });
+      if (filteredPages.length > 0) {
+        return { ...page, pages: filteredPages };
+      } else if (regex.test(page.title)) {
+        return page.url ? { ...page, pages: [] } : page;
+      }
+
+      return null;
+    })
+    .filter(Boolean);
 };
 
 const Navigation = ({ className, searchTerm }) => {
