61a97fb9a966c07ed1a681ef1aad820a8c76f3c5
pachi <pachicodes@gmail.com>
2021-01-22 15:43:10 -0500

    added content to builders page v1

--- src/pages/developer-builders.js
+++ src/pages/developer-builders.js
@@ -7,35 +7,73 @@ import ExternalLink from '../components/ExternalLink';
 import FeatherIcon from '../components/FeatherIcon';
 import devChampionHeader from '../images/developer-champion/developer-champions.jpg';
 import uptimeEverythingHeader from '../images/developer-champion/uptime-everything-header-image.jpg';
-import styles from './developer-champion.module.scss';
+import styles from './developer-builders.module.scss';
 
 const DeveloperChampionPage = () => {
   return (
     <>
       <SEO />
       <PageLayout type={PageLayout.TYPE.SINGLE_COLUMN}>
-        <PageLayout.Header title="New Relic Developer Champions" />
+        <PageLayout.Header title="New Relic Builders" />
         <PageLayout.Content>
           <section className={cx(styles.section, styles.twoColumn)}>
             <div>
               <p>
-                New Relic Champions are the voice of the developer community. As
-                experts and innovators, they are given the resources to not only
-                share the newest product innovations and updates but also to
-                provide feedback of the community back to New Relic product and
-                engineering teams.
+                Builder is a simple yet meaningful word. Building things is on a
+                developer’s nature independently of their tech stack and
+                experience level.{' '}
               </p>
               <p>
-                Champions solve big problems using New Relic as their toolkit
-                and are recognized as experts and leaders in the New Relic
-                technical community.{' '}
+                {' '}
+                The Reliquary(link), our developer community, brings together
+                experienced developers/DevOps people with those who are new to
+                software development.
               </p>
+              <p> The Reliquary’s goal is to be a long term home that:</p>
+              <ul>
+                <li>
+                  Delivers a warm welcome to everyone, regardless of their
+                  background or current ability.
+                </li>
+                <li>
+                  Recognizes that each person is on an individual learning path.{' '}
+                </li>
+                <li>
+                  Helps everyone to find the right way to meet their personal
+                  learning goals.{' '}
+                </li>
+                <li>Respects each person’s potential to contribute. </li>
+                <li>
+                  Uses awards and recognition to help people feel a sense of
+                  achievement, rather than to gate keep.{' '}
+                </li>
+                <li>
+                  Combines warmth and friendliness with being technically
+                  correct and being friendly.
+                </li>
+              </ul>
+              <p>The New Relic Builders is vital to achieve these goals. </p>
+              <p>
+                {' '}
+                These special members of our community will be there to welcome
+                new members, helping them feel at home, guide people in their
+                tech journeys with their experience and advice, and share with
+                the community what they know, helping everyone around to level
+                up.
+              </p>
+              <p>
+                Builders are developers who want to be part of this community
+                and share our goals, have expertise to educate other developers,
+                and are looking for a platform to be heard and make a
+                difference.
+              </p>
+
               <Button
                 as={ExternalLink}
                 variant={Button.VARIANT.PRIMARY}
                 href="https://forms.gle/Zkdub5e1x4MNqSKW9"
               >
-                Nominate a developer champion
+                Apply to be a Builder
                 <FeatherIcon
                   className={styles.externalLinkIcon}
                   name="external-link"
@@ -49,15 +87,12 @@ const DeveloperChampionPage = () => {
             />
           </section>
           <section className={styles.section}>
-            <h2>What do Developer Champions do?</h2>
+            <h2>Why be a New Relic Builder?</h2>
             <p>
-              New Relic Champions demonstrate expertise in using New Relic
-              products by solving large problems and positioning New Relic as a
-              central force in their strategies. The New Relic Champions is a
-              recognition and partnership program designed to acknowledge the
-              developers that are driving innovation within their companies and
-              making top contributions to the developer community.They also
-              commit to making their work public by:
+              Being a New Relic builder means that you are a part of a group
+              that cares about their community and wants to help them to grow
+              and thrive. You like to build things, learn and share your
+              expertise with others.
             </p>
           </section>
           <section className={cx(styles.section, styles.championProgram)}>
@@ -67,7 +102,7 @@ const DeveloperChampionPage = () => {
                 name="github"
                 size="4rem"
               />
-              <h4>Open-source contributions</h4>
+              <h4>Learn the Craft</h4>
               <p>
                 Serving as an open-source author or maintainer for an accepted
                 public project related to New Relic One
@@ -79,7 +114,7 @@ const DeveloperChampionPage = () => {
                 name="trello"
                 size="4rem"
               />
-              <h4>Content creation</h4>
+              <h4>Build your personal Brand</h4>
               <p>
                 Authoring two pieces of content in the New Relic Explorers Hub /
                 Dev website
@@ -91,7 +126,7 @@ const DeveloperChampionPage = () => {
                 name="users"
                 size="4rem"
               />
-              <h4>Community engagement</h4>
+              <h4>Mentor Others</h4>
               <p>
                 Delivering and/or organizing two events focused on an
                 observability platform theme in which New Relic plays a crucial
@@ -104,7 +139,7 @@ const DeveloperChampionPage = () => {
               href="https://forms.gle/Zkdub5e1x4MNqSKW9"
               variant={Button.VARIANT.PRIMARY}
             >
-              Nominate a Developer Champion{' '}
+              I want in!{' '}
               <FeatherIcon
                 name="external-link"
                 className={styles.externalLinkIcon}
@@ -113,42 +148,42 @@ const DeveloperChampionPage = () => {
           </section>
           <section className={cx(styles.section, styles.twoColumn)}>
             <div>
-              <h2>Why should you join and how will we support?</h2>
+              <h2>What are the Builder's responsabilities?</h2>
               <p>
-                As a benefit of being a Developer Champion, New Relic provides
-                unique access to our Developer Advocacy team and the resources
-                of our product organization, as well as specialized recognition
-                and rewards.
+                As a New Relic Builder, we will expect you to create content to
+                meet the program's needs, and you will get the help and guidance
+                from our DevRel team on that. Be ready to mentor other members
+                of our community, both in their technical growth as well as
+                content production. Last but not least, you will be expected to
+                to make and uphold commitments related to the program
               </p>
-              <h2>Developer Champions benefits:</h2>
+              <h2>What are the Requisits? *</h2>
+
               <ul>
+                <h4>The Must haves:</h4>
+                <li>Be comfortable creating content in English</li>
+                <li>Have expertise in your tech stack of choice</li>
                 <li>
-                  Formal, specialized access to the New Relic Product
-                  organization
-                  <ul>
-                    <li>
-                      Champions have direct access to the New Relic’s Developer
-                      Ecosystem team
-                    </li>
-                    <li>Custom badge to wear with pride at events</li>
-                  </ul>
+                  Track record of producing engaging developer education
+                  materials
                 </li>
                 <li>
-                  Public recognition on the New Relic Developer website and
-                  badging in the New Relic Explorers Hub as a Champion
+                  No history of community conflict, CoC violations, or other
+                  problematic behavior
                 </li>
-                <li>Exclusive Champion-only swag</li>
-                <li>
-                  Early access program for some of our products (under NDA)
-                </li>
-                <li>
-                  Priority access to off-site FutureHack events (including when
-                  Lew is participating)
-                </li>
-                <li>
-                  Increased Explorer’s Hub support SLA Access to private
-                  Developer Champion Explorer’s Hub group
-                </li>
+                <li>Commitment to the values of the program</li>
+              </ul>
+
+              <ul>
+                <h4>It is nice to have:</h4>
+                <li>Be Active on The Reliquary</li>
+                <li>Have experience using New Relic products</li>
+              </ul>
+
+              <ul>
+                <h4>You get extra brownie points if:</h4>
+                <li>You have an existing audience</li>
+                <li>Are weill known in other tech communities</li>
               </ul>
             </div>
             <img
