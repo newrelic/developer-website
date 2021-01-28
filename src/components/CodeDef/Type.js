2dfba953af27cd311c94236adcfed9e9be5327a3
Jerel Miller <jerelmiller@gmail.com>
2021-01-28 01:14:43 -0800

    chore: use emotion to style codedef

--- src/components/CodeDef/Type.js
+++ src/components/CodeDef/Type.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './CodeDef.module.scss';
+import { css } from '@emotion/core';
 
-const Type = ({ children }) => <span className={styles.type}>{children}</span>;
+const Type = ({ className, children }) => (
+  <span
+    className={className}
+    css={css`
+      padding: 0.125rem;
+      border-radius: 0.125rem;
+      color: var(--color-green-500);
+      background: var(--color-green-050);
+      font-family: var(--code-font);
+
+      .dark-mode & {
+        color: var(--color-green-600);
+        background: var(--color-green-100);
+      }
+    `}
+  >
+    {children}
+  </span>
+);
 
 Type.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.node,
 };
 
