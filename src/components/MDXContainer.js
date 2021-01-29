6cc7100c49a2d98929dec09009b6935ea9264c4c
Jerel Miller <jerelmiller@gmail.com>
2021-01-28 22:15:41 -0800

    chore: update MDXContainer to use new MDX component

--- src/components/MDXContainer.js
+++ src/components/MDXContainer.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
-import { MDXProvider } from '@mdx-js/react';
+import { MarkdownContainer, MDX } from '@newrelic/gatsby-theme-newrelic';
 
 import Intro from './Intro';
 import Iframe from './Iframe';
@@ -10,40 +8,25 @@ import Tutorial from './Tutorial';
 import TutorialStep from './TutorialStep';
 import TutorialSection from './TutorialSection';
 import Project from './Project';
-import {
-  Callout,
-  MDXCodeBlock,
-  Table,
-  Video,
-} from '@newrelic/gatsby-theme-newrelic';
-
-import styles from './MDXContainer.module.scss';
 
 const components = {
-  Callout,
-  Video,
   // Remove these when all step/steps components have been updated to the new
   // name
   Step: TutorialStep,
   Steps: TutorialSection,
   Project,
-  Table,
   Tutorial,
   TutorialStep,
   TutorialSection,
   Intro,
   iframe: Iframe,
-  code: MDXCodeBlock,
-  pre: (props) => props.children,
 };
 
 const MDXContainer = ({ className, children }) => {
   return (
-    <div className={cx(styles.container, className)}>
-      <MDXProvider components={components}>
-        <MDXRenderer>{children}</MDXRenderer>
-      </MDXProvider>
-    </div>
+    <MarkdownContainer className={className}>
+      <MDX body={children} components={components} />
+    </MarkdownContainer>
   );
 };
 
