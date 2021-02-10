b3add85ab70be4c200462c793f99c437adca4558
Jemiah Sius <jsius@newrelic.com>
2021-02-10 15:29:42 -0500

    Update content

--- src/pages/nerd-log.js
+++ src/pages/nerd-log.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 import cx from 'classnames';
 import PageLayout from '../components/PageLayout';
+import MarketoForm from '../components/MarketoForm';
 import { Button, NewRelicLogo } from '@newrelic/gatsby-theme-newrelic';
 import SEO from '../components/Seo';
 import { graphql, useStaticQuery, Link } from 'gatsby';
@@ -10,6 +11,7 @@ import FeatherIcon from '../components/FeatherIcon';
 import styles from './nerd-log.module.scss';
 import { teamMembers } from '../data/nerdlogData';
 import graphLines from '../images/nerd-days/hero-image-graph-lines.png';
+import nerdlogBanner from '../images/nerdlog/nerdlog-banner.png';
 import heroBackgroundPattern from '../images/nerd-days/nerd-days-hero-bg-pattern.png';
 import styled from '@emotion/styled';
 
@@ -57,116 +59,12 @@ const NerdlogPage = () => {
       <SEO />
       <PageLayout type={PageLayout.TYPE.SINGLE_COLUMN}>
         <PageLayout.Content>
-          <section
-            css={css`
-              margin-top: -2rem;
-              padding-bottom: 2rem;
-              text-align: center;
-              background-image: url(${graphLines}),
-                url(${heroBackgroundPattern}),
-                linear-gradient(165deg, #06b4c7 10%, #0269ce 60%);
-              background-size: 104%, 13%, 100%;
-              background-position: -24px -10px, 0, 0;
-              background-blend-mode: normal, soft-light, normal;
-              border-radius: 20px;
-              background-repeat: no-repeat, repeat, no-repeat;
-
-              .dark-mode & {
-                position: relative;
-                background-image: url(${graphLines}),
-                  url(${heroBackgroundPattern}),
-                  linear-gradient(145deg, #01273d 20%, #2dcde3 250%);
-                background-blend-mode: soft-light, soft-light, normal;
-              }
-
-              ${breakpoints.laptop} {
-                background-position: -14px -10px, 0, 0;
-              }
-
-              ${breakpoints.mobile} {
-                border-radius: 0;
-                margin: -48px -32px 0 -32px;
-              }
-            `}
-          >
-            <div
-              css={css`
-                display: inline-block;
-                max-width: 364px;
-                padding: 0.5rem 1.25rem;
-                margin: 0 48px;
-                text-align: center;
-                background-color: #fff;
-                box-shadow: 0px 18px 9px rgba(13, 80, 138, 0.12),
-                  0px 7.51997px 3.75998px rgba(13, 80, 138, 0.0862625),
-                  0px 4.02054px 2.01027px rgba(13, 80, 138, 0.0715329),
-                  0px 2.25388px 1.12694px rgba(13, 80, 138, 0.06),
-                  0px 1.19702px 0.598509px rgba(13, 80, 138, 0.0484671),
-                  0px 0.498106px 0.249053px rgba(13, 80, 138, 0.0337375);
-                border-radius: 0px 0px 20px 20px;
-
-                .dark-mode & {
-                  background-color: rgba(0, 0, 0, 0.5);
-                  backdrop-filter: blur(5px);
-                }
-              `}
-            >
-              <NewRelicLogo />
-            </div>
-            <h3
-              css={css`
-                max-width: 566px;
-                margin: 2rem auto 0.5rem;
-                color: #fff;
-                font-style: normal;
-                font-weight: 600;
-                font-size: 30px;
-                line-height: 2.5rem;
-                text-align: center;
-
-                ${breakpoints.laptop} {
-                  padding: 0 3rem;
-                  margin: 1.75rem auto 1.25rem;
-                  font-size: 1.5rem;
-                  line-height: 2.25rem;
-                }
-
-                ${breakpoints.mobile} {
-                  padding: 0 ${layout.contentPadding};
-                  font-size: 1.25rem;
-                  line-height: 1.75rem;
-                }
-              `}
-            >
-              Nerdlog
-            </h3>
-            <h4
-              css={css`
-                max-width: 566px;
-                margin: 1rem auto 1.5rem;
-                color: #fff;
-                font-style: normal;
-                font-weight: 600;
-                font-size: 25px;
-                line-height: 2.5rem;
-                text-align: center;
-
-                ${breakpoints.laptop} {
-                  padding: 0 3rem;
-                  margin: 1.75rem auto 1.25rem;
-                  font-size: 1.5rem;
-                  line-height: 2.25rem;
-                }
-
-                ${breakpoints.mobile} {
-                  padding: 0 ${layout.contentPadding};
-                  font-size: 1.25rem;
-                  line-height: 1.75rem;
-                }
-              `}
-            >
-              A changelog for the modern developer
-            </h4>
+          <section>
+            <img
+              src={nerdlogBanner}
+              className={cx(styles.nerdlogBanner)}
+              alt="Nerdlog banner"
+            />
           </section>
           <Section>
             <SectionHeading>
@@ -187,9 +85,8 @@ const NerdlogPage = () => {
               <div>
                 <h2>What is the Nerdlog?</h2>
                 <p>
-                  The Nerdlog is your opportunity to learn from New Relic
-                  experts live or on-demand. We want your teams to stay informed
-                  and navigate New Relic One like a pro.
+                  The Nerdlog is our brand new live-stream changelog on Twitch.
+                  Every Thursday, you can:
                 </p>
                 <p>
                   <strong>
@@ -197,40 +94,62 @@ const NerdlogPage = () => {
                   </strong>
                   <ul>
                     <li>
-                      Showcase the latest features, capabilities, and
-                      integrations
+                      <strong>
+                        Learn about the latest features from the people who
+                        built them
+                      </strong>
+                      <br />
+                      Watch New Relic product managers and engineers play with
+                      the latest features and integrations to help you navigate
+                      New Relic One like a pro.
                     </li>
                     <li>
-                      Explain the most recent changes and answer your most
-                      burning questions
+                      <strong>
+                        Get some knowledge, a little inspiration, and a whole
+                        lot of answers
+                      </strong>
+                      <br />
+                      Walk away with tons of resources, projects with
+                      step-by-step instructions, and all your questions
+                      answered.
                     </li>
                     <li>
-                      Share best practices, resources, and hands-on projects{' '}
+                      <strong>Connect with other engineers </strong>
+                      <br />
+                      experiences with other New Relic users and have fun as you
+                      learn about observability.{' '}
                     </li>
                   </ul>
                 </p>
-                <p>
-                  We want to connect with you, learn from you, and continue
-                  building products with you in mind.
-                </p>
                 <h3>Save the date &amp; join us online</h3>
-                <p>We are live every Thursday!</p>
                 <p>
-                  <strong>Where:</strong> Follow us on Twitch
+                  <strong>We are live every Thursday!</strong>
+                </p>
+                <p>
+                  <strong>Where:</strong> Follow us on{' '}
+                  <a href="https://www.twitch.tv/new_relic">Twitch</a>
                   <br />
                   <strong>Date:</strong> 12 p.m. PT
                 </p>
                 <p>
-                  Recordings are available on{' '}
-                  <a href="https://youtube.com">YouTube.</a>
+                  Can’t make it? We’ll miss you, but recordings are available on{' '}
+                  <a href="https://www.twitch.tv/new_relic/videos">Twitch.</a>
                 </p>
               </div>
               <div>
                 <h3>Get Reminders</h3>
                 <p>
-                  Enter your email address to get notified before our next live
-                  stream and information about our previous ones.
+                  Enter your email address to get notified before our next
+                  episode of the Nerdlog and information about our previous
+                  ones.
                 </p>
+                <MarketoForm
+                  id={4783}
+                  title="Stay up-to-date with Nerdlog"
+                  munchkinId="412-MZS-894"
+                  publishableKey="pk_4d10daa544de6f993a9a9ce002ccd1c6"
+                  redirectLink="/nerd-days-thank-you"
+                />
               </div>
             </div>
           </Section>
