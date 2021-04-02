61aa1e6a5ce178e5f5d9758ae667d334cfa9f96d
John P Vajda <jvajda@newrelic.com>
2021-04-02 16:00:35 -0600

    chore: addes tessen configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,14 +16,13 @@ module.exports = {
     {
       resolve: '@newrelic/gatsby-theme-newrelic',
       options: {
-        gaTrackingId: 'UA-3047412-33',
         layout: {
           contentPadding: '2rem',
           maxWidth: '1700px',
           component: require.resolve('./src/layouts'),
           mobileBreakpoint: '760px',
         },
-       prism: {
+        prism: {
           languages: ['yaml', 'sass', 'scss', 'java'],
         },
         splitio: {
@@ -94,6 +93,20 @@ module.exports = {
               beacon: 'staging-bam-cell.nr-data.net',
               errorBeacon: 'staging-bam-cell.nr-data.net',
             },
+          },
+        },
+        tessen: {
+          product: 'DEV',
+          subproduct: 'TDEV',
+          segmentWriteKey: 'AEfP8c1VSuFxhMdk3jYFQrYQV9sHbUXx',
+          trackPageViews: true,
+          pageView: {
+            name: 'pageView',
+            category: 'DocPageView',
+            getProperties: ({ location, env }) => ({
+              path: location.pathname,
+              env: env === 'production' ? 'prod' : env,
+            }),
           },
         },
       },
