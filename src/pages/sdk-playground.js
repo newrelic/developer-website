1e03882b68976f86dcbbf3f75e4f3afda87ffc3c
Cayla Hamann <chamann@newrelic.com>
2021-04-05 17:12:07 -0400

    chore: remove unused vars

--- src/pages/sdk-playground.js
+++ src/pages/sdk-playground.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ComponentPlayground from '../components/Playground/ComponentPlayground';
-import VisualizationPlayground from '../components/Playground/VisualizationPlayground';
 
 const SdkPlayground = () => {
   return <ComponentPlayground />;
