08e65feaf7418b81586e490463fd2abfb2091098
lbaker <lizbaker.pdx@gmail.com>
2021-04-23 16:39:00 -0700

    chore: Create sdk loading hook

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,3 +7,6 @@ export const SPLITS = {
 export const SPLIT_TRACKING_EVENTS = {
   RELATED_CONTENT_ACTION_CLICKED: 'related_content.contribute_action_clicked',
 };
+
+export const SDK_BASE_URL =
+  'https://d1zobbh8kytrtv.cloudfront.net/platform/wanda--wanda-ec-ui--nr1-docs';
