2d09ccfe0c10ba93a6f430a22e0a8f133ecfbf81
Clinton <clinton.langosch@gmail.com>
2021-06-02 11:45:05 -0700

    wip: add top search and search tools

--- src/pages/observability-packs.js
+++ src/pages/observability-packs.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import DevSiteSeo from '../components/DevSiteSeo';
+import { css } from '@emotion/react';
 import PackTile from '../components/PackTile';
 import PackList from '../components/PackList';
-import { Surface } from '@newrelic/gatsby-theme-newrelic';
+import { SearchInput, Button } from '@newrelic/gatsby-theme-newrelic';
 import * as styles from './observability-packs.module.scss';
 
 const LIST_VIEWS = {
@@ -13,32 +14,54 @@ const LIST_VIEWS = {
 
 const ObservabilityPacksPage = ({ data, listView }) => {
   const {
-    allO11YPacks: {
-      nodes: [{ observabilityPacks }],
-    },
+    allObservabilityPacks: { nodes: o11yPacks },
   } = data;
 
   return (
-    <div>
-      <h1>Observability Packs Landing</h1>
-      <PackList>
-        {observabilityPacks.map((pack) => {
-          const imgSrc = pack.dashboards?.[0]?.screenshots?.[0];
-          return (
-            <PackTile
-              name={pack.name}
-              description={pack.description}
-              featuredImageUrl={
-                imgSrc || 'https://via.placeholder.com/400x275.png?text=Image'
-              }
-              to={`/observability-packs/${pack.name}/${pack.id}`}
-            >
-              {pack.name}
-            </PackTile>
-          );
-        })}
-      </PackList>
-    </div>
+    <>
+      <SearchInput
+        size={SearchInput.SIZE.LARGE}
+        width={'100%'}
+        css={css`
+          margin: 15px 0;
+        `}
+        onClear={() => console.log('clear me')}
+      />
+      <div
+        css={css`
+          background-color: var(--color-neutrals-100);
+          margin: 15px 0;
+          height: 75px;
+          .dark-mode & {
+            background-color: var(--color-dark-100);
+          }
+        `}
+      >
+        <span>Showing {o11yPacks.length} results</span>
+        <div>
+          <span>Sort by</span>
+        </div>
+      </div>
+      <div>
+        <PackList>
+          {o11yPacks.map((pack) => {
+            const imgSrc = pack.dashboards?.[0]?.screenshots?.[0];
+            return (
+              <PackTile
+                name={pack.name}
+                description={pack.description}
+                featuredImageUrl={
+                  imgSrc || 'https://via.placeholder.com/400x275.png?text=Image'
+                }
+                to={`/observability-packs/${pack.name}/${pack.id}`}
+              >
+                {pack.name}
+              </PackTile>
+            );
+          })}
+        </PackList>
+      </div>
+    </>
   );
 };
 
@@ -53,29 +76,27 @@ ObservabilityPacksPage.propTypes = {
 
 export const pageQuery = graphql`
   query {
-    allO11YPacks {
+    allObservabilityPacks {
       nodes {
-        observabilityPacks {
-          id
-          name
-          website
-          logo
-          level
-          dashboards {
-            description
-            name
-            screenshots
-            url
-          }
-          alerts {
-            definition
-            name
-            url
-          }
-          authors
+        id
+        name
+        website
+        logo
+        level
+        dashboards {
           description
-          icon
+          name
+          screenshots
+          url
         }
+        alerts {
+          definition
+          name
+          url
+        }
+        authors
+        description
+        icon
       }
     }
   }
