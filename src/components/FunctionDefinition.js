c44af4379f9ebdbe83c9f54af6c00eaa149a3700
Zack Stickles <zstickles@gmail.com>
2021-06-17 15:00:51 -0700

    chore: cleaning up indent levels

--- src/components/FunctionDefinition.js
+++ src/components/FunctionDefinition.js
@@ -4,37 +4,37 @@ import CodeDef from './CodeDef';
 import { graphql } from 'gatsby';
 
 const FunctionDefinition = ({ className, arguments: params, returnValue }) => {
+  if (!params.length && !returnValue.length) {
+    return null;
+  }
+
   return (
-    (params.length > 0 || returnValue.length > 0) && (
-      <CodeDef className={className}>
-        <CodeDef.Keyword>function</CodeDef.Keyword>{' '}
-        <CodeDef.Bracket>{params.length > 0 ? '(' : '()'}</CodeDef.Bracket>
-        {params.length > 0 && (
-          <CodeDef.Block>
-            {params.map((param, i) => (
-              <div key={i}>
-                <CodeDef.Identifier>
-                  {param.type.startsWith('...')
-                    ? `...${param.name}`
-                    : param.name}
-                  :{' '}
-                </CodeDef.Identifier>
-                <CodeDef.Type>{param.type}</CodeDef.Type>
-                {i !== params.length - 1 ? ', ' : ' '}
-                <CodeDef.Comment text={param.description} />
-              </div>
-            ))}
-          </CodeDef.Block>
-        )}
-        {params.length > 0 && <CodeDef.Bracket>)</CodeDef.Bracket>}
-        {returnValue.length > 0 && (
-          <>
-            <CodeDef.Operator> => </CodeDef.Operator>
-            <CodeDef.Type>{returnValue[0].type}</CodeDef.Type>
-          </>
-        )}
-      </CodeDef>
-    )
+    <CodeDef className={className}>
+      <CodeDef.Keyword>function</CodeDef.Keyword>{' '}
+      <CodeDef.Bracket>{params.length > 0 ? '(' : '()'}</CodeDef.Bracket>
+      {params.length > 0 && (
+        <CodeDef.Block>
+          {params.map((param, i) => (
+            <div key={i}>
+              <CodeDef.Identifier>
+                {param.type.startsWith('...') ? `...${param.name}` : param.name}
+                :{' '}
+              </CodeDef.Identifier>
+              <CodeDef.Type>{param.type}</CodeDef.Type>
+              {i !== params.length - 1 ? ', ' : ' '}
+              <CodeDef.Comment text={param.description} />
+            </div>
+          ))}
+        </CodeDef.Block>
+      )}
+      {params.length > 0 && <CodeDef.Bracket>)</CodeDef.Bracket>}
+      {returnValue.length > 0 && (
+        <>
+          <CodeDef.Operator> => </CodeDef.Operator>
+          <CodeDef.Type>{returnValue[0].type}</CodeDef.Type>
+        </>
+      )}
+    </CodeDef>
   );
 };
 
