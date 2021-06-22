62a0538b65cb812354800764e1fcfbc4b8d65e37
Clinton <clinton.langosch@gmail.com>
2021-06-22 10:26:08 -0700

    fix: fix lint error on alt attribute

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -122,7 +122,7 @@ const ImageSlider = ({ images, height }) => {
               >
                 <img
                   src={item}
-                  alt={`Image ${item.split('/').slice(-1)}`}
+                  alt={`${item.split('/').slice(-1)}`}
                   css={css`
                     height: ${height}px;
                     width: 100%;
