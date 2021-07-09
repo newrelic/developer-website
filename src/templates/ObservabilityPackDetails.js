fb03414ea082db7ca74665e472d214419cbd27eb
Alec Swanson <aswanson@newrelic.com>
2021-07-09 11:49:25 -0400

    feat: add empty state for pack details

--- src/templates/ObservabilityPackDetails.js
+++ src/templates/ObservabilityPackDetails.js
@@ -37,6 +37,170 @@ const allowedElements = [
   'hr',
 ];
 
+const renderDashboards = (dashboards) => {
+  const content = dashboards.map((dashboard) => (
+    <>
+      <h3>{dashboard.name}</h3>
+      <ImageSlider height={400} images={dashboard.screenshots} />
+      {dashboard.description && (
+        <>
+          <h4>Description</h4>
+          <p>{dashboard.description}</p>
+        </>
+      )}
+    </>
+  ));
+
+  return (
+    <>
+      <Intro
+        css={css`
+          margin-bottom: 16px;
+        `}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Intro>
+      {content}
+    </>
+  );
+};
+
+const renderAlerts = (alerts) => {
+  const alertContent = alerts.map((alert) => (
+    <>
+      <h3>{alert.name}</h3>
+      {alert.description && (
+        <>
+          <h4>Description</h4>
+          <p>{alert.description}</p>
+        </>
+      )}
+    </>
+  ));
+
+  return (
+    <>
+      <Intro
+        css={css`
+          margin-bottom: 16px;
+        `}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Intro>
+      {alertContent}
+    </>
+  );
+};
+
+const renderSynthetics = () => {
+  return (
+    <Intro
+      css={css`
+        margin-bottom: 16px;
+      `}
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </Intro>
+  );
+};
+
+const renderVisualizations = () => {
+  return (
+    <Intro
+      css={css`
+        margin-bottom: 16px;
+      `}
+    >
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </Intro>
+  );
+};
+
+const emptyStateContent = (pack) => {
+  return (
+    <div
+      css={css`
+        border: 1px solid var(--divider-color);
+        border-radius: 0.25rem;
+        padding: 1rem;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          padding: 2rem;
+        `}
+      >
+        <Icon
+          css={css`
+            font-size: 4rem;
+            color: var(--divider-color);
+          `}
+          name="edit"
+        />
+      </div>
+      <p
+        css={css`
+          text-align: center;
+        `}
+      >
+        I'm baby quinoa DIY narwhal artisan organic slow-carb cliche twee. Kogi
+        YOLO meggings quinoa affogato vegan bespoke hashtag. VHS skateboard palo
+        santo, gastropub edison bulb asymmetrical humblebrag plaid disrupt.
+        Activated charcoal glossier kinfolk before they sold out pok pok quinoa
+        bicycle rights humblebrag fanny pack church-key enamel pin.
+      </p>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+        `}
+      >
+        <Button
+          as={Link}
+          variant={Button.VARIANT.PRIMARY}
+          to={getPackUrl(pack.logoUrl)}
+          rel="noopener noreferrer"
+          instrumentation={{ packName: pack.name }}
+        >
+          <Icon
+            name="fe-github"
+            css={css`
+              margin-right: 7px;
+            `}
+          />
+          View Repo
+        </Button>
+      </div>
+    </div>
+  );
+};
+
 const ObservabilityPackDetails = ({ data, location }) => {
   const pack = data.observabilityPacks;
   const tessen = useTessen();
@@ -86,30 +250,17 @@ const ObservabilityPackDetails = ({ data, location }) => {
             `}
           >
             <Tabs.BarItem id="overview">Overview</Tabs.BarItem>
-            <Tabs.BarItem
-              id="dashboards"
-              disabled={!(pack.dashboards?.length ?? 0)}
-              count={pack.dashboards?.length ?? 0}
-            >
+            <Tabs.BarItem id="dashboards" count={pack.dashboards?.length ?? 0}>
               Dashboards
             </Tabs.BarItem>
-            <Tabs.BarItem
-              id="alerts"
-              disabled={!(pack.alerts?.length ?? 0)}
-              count={pack.alerts?.length ?? 0}
-            >
+            <Tabs.BarItem id="alerts" count={pack.alerts?.length ?? 0}>
               Alerts
             </Tabs.BarItem>
-            <Tabs.BarItem
-              id="synthetics"
-              disabled={!(pack.synthetics?.length ?? 0)}
-              count={pack.synthetics?.length ?? 0}
-            >
+            <Tabs.BarItem id="synthetics" count={pack.synthetics?.length ?? 0}>
               Synthetics
             </Tabs.BarItem>
             <Tabs.BarItem
               id="visualizations"
-              disabled={!(pack.visualizations?.length ?? 0)}
               count={pack.visualizations?.length ?? 0}
             >
               Visualizations
@@ -124,124 +275,23 @@ const ObservabilityPackDetails = ({ data, location }) => {
                   {pack.description}
                 </Markdown>
               </Tabs.Page>
-              <Tabs.Page id="requirements">
-                <Intro
-                  css={css`
-                    margin-bottom: 16px;
-                  `}
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Intro>
-              </Tabs.Page>
               <Tabs.Page id="dashboards">
-                <Intro
-                  css={css`
-                    margin-bottom: 16px;
-                  `}
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Intro>
-                {pack.dashboards?.map((dashboard) => (
-                  <>
-                    <h3>{dashboard.name}</h3>
-                    <ImageSlider height={400} images={dashboard.screenshots} />
-                    {dashboard.description && (
-                      <>
-                        <h4>Description</h4>
-                        <p>{dashboard.description}</p>
-                      </>
-                    )}
-                  </>
-                ))}
+                {pack.dashboards
+                  ? renderDashboards(pack.dashboards)
+                  : emptyStateContent(pack)}
               </Tabs.Page>
               <Tabs.Page id="alerts">
-                <Intro
-                  css={css`
-                    margin-bottom: 16px;
-                  `}
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Intro>
-                {pack.alerts?.map((alert) => (
-                  <>
-                    <h3>{alert.name}</h3>
-                    {alert.description && (
-                      <>
-                        <h4>Description</h4>
-                        <p>{alert.description}</p>
-                      </>
-                    )}
-                  </>
-                ))}
+                {pack.alerts
+                  ? renderAlerts(pack.alerts)
+                  : emptyStateContent(pack)}
               </Tabs.Page>
               <Tabs.Page id="synthetics">
-                <Intro
-                  css={css`
-                    margin-bottom: 16px;
-                  `}
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Intro>
+                {pack.synthetics ? renderSynthetics() : emptyStateContent(pack)}
               </Tabs.Page>
               <Tabs.Page id="visualizations">
-                <Intro
-                  css={css`
-                    margin-bottom: 16px;
-                  `}
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Intro>
-              </Tabs.Page>
-              <Tabs.Page id="nerdpacks">
-                <Intro
-                  css={css`
-                    margin-bottom: 16px;
-                  `}
-                >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </Intro>
+                {pack.visualizations
+                  ? renderVisualizations()
+                  : emptyStateContent(pack)}
               </Tabs.Page>
             </Tabs.Pages>
           </Layout.Content>
@@ -335,5 +385,3 @@ export const pageQuery = graphql`
     }
   }
 `;
-
-export default ObservabilityPackDetails;
