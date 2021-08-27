93f78175ae8189ccd694b1cc1a64c6758e69d610
Clinton <clinton.langosch@gmail.com>
2021-08-27 15:29:44 -0700

    chore: apply full width style for details page w/out sidebar

--- src/layouts/QuickStartLayout.js
+++ src/layouts/QuickStartLayout.js
@@ -6,15 +6,22 @@ import {
 } from '@newrelic/gatsby-theme-newrelic';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/react';
+import { useLocation } from '@reach/router';
 import '../components/styles.scss';
 
 const QuickStartLayout = ({ children }) => {
+  const location = useLocation();
+  const isLanding = location.pathname === '/observability-packs/';
   return (
     <>
       <GlobalHeader />
       <MobileHeader>New sidebar here</MobileHeader>
-      <Layout>
-        <Layout.Sidebar>New sidebar here</Layout.Sidebar>
+      <Layout
+        css={css`
+          --sidebar-width: ${isLanding ? '300px' : '0'};
+        `}
+      >
+        {isLanding && <Layout.Sidebar>New sidebar here</Layout.Sidebar>}
         <Layout.Main
           css={css`
             min-height: 100vh;
