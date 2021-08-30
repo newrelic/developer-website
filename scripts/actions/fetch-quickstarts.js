35977e238fdf50e0a078244a1dcc40faa663d56c
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-08-30 15:10:38 -0700

    refactor: swap a few more instances of pack -> quickstart
    * replace usage of pack in variables, logs in fetch-quickstarts script
    * update test(s) to use quickstart terminology and account for recent
    changes made

--- scripts/actions/fetch-quickstarts.js
+++ scripts/actions/fetch-quickstarts.js
@@ -13,11 +13,11 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 const get = require('lodash.get');
 
-const PACKS_FILE_PATH = './src/data/quickstarts.json';
+const QUICKSTARTS_FILE_PATH = './src/data/quickstarts.json';
 const NR_API_URL = process.env.NR_API_URL;
 const NR_API_TOKEN = process.env.NR_API_TOKEN;
 
-const packQuery = `# gql 
+const quickstartQuery = `# gql 
   {
     docs {
       openInstallation {
@@ -67,7 +67,7 @@ const packQuery = `# gql
  * @returns {Promise<Object[]|undefined>} returns the resulting array
  * or `undefined` if there was an error
  **/
-const fetchPacks = async (queryString, url, token) => {
+const fetchQuickstarts = async (queryString, url, token) => {
   try {
     const res = await fetch(url, {
       method: 'post',
@@ -105,21 +105,21 @@ const validateEnvVars = () => {
 };
 
 /*
- * @param {String} query a graphql query for observability packs
+ * @param {String} query a graphql query for quickstarts
  * @param {String} url the New Relic API endpoint
  * @param {String} token a New Relic API token
  **/
 const main = async (query, url, token) => {
-  const results = await fetchPacks(query, url, token);
+  const results = await fetchQuickstarts(query, url, token);
 
   if (results) {
-    const packs = results.quickstarts;
-    console.log(`Found ${packs.length} packs.`);
-    console.log(`Writing ${PACKS_FILE_PATH}`);
-    fs.writeFileSync(PACKS_FILE_PATH, JSON.stringify(packs, null, 2));
+    const quickstarts = results.quickstarts;
+    console.log(`Found ${quickstarts.length} quickstarts.`);
+    console.log(`Writing ${QUICKSTARTS_FILE_PATH}`);
+    fs.writeFileSync(QUICKSTARTS_FILE_PATH, JSON.stringify(quickstarts, null, 2));
   } else {
     console.log(
-      'No packs were returned from the api, check the logs for errors.'
+      'No quickstarts were returned from the api, check the logs for errors.'
     );
     if (require.main === module) {
       process.exit(1);
@@ -129,7 +129,7 @@ const main = async (query, url, token) => {
 
 if (require.main === module) {
   validateEnvVars();
-  main(packQuery, NR_API_URL, NR_API_TOKEN);
+  main(quickstartQuery, NR_API_URL, NR_API_TOKEN);
 }
 
 module.exports = main;
