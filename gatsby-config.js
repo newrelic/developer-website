ee5ef74abaa8a0701672805521c5e1eee3323299
Clinton <clinton.langosch@gmail.com>
2021-09-17 16:47:36 -0700

    feat: bump theme and update tessen
    - updates tessen by adding theme option
    - removes old 1.3.0 tessen file for cleanliness

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,6 +107,7 @@ module.exports = {
           },
         },
         tessen: {
+          tessenVersion: '1.14.0',
           product: 'DEV',
           subproduct: 'TDEV',
           segmentWriteKey: 'Ako0hclX8WGHwl9rm4n5uxLtT4wgEtuU',
