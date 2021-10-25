1cad846c64785a1757fd4fc40ca71c7ba844accb
lbaker <lizbaker.pdx@gmail.com>
2021-10-25 10:39:51 -0700

    chore: Fix autofilled css

--- src/pages/instant-observability.js
+++ src/pages/instant-observability.js
@@ -317,7 +317,7 @@ const QuickstartsPage = ({ data, location }) => {
                     <div
                       css={css`
                         display: flex;
-                        width: 100%auto;
+                        width: 100%;
                         align-items: center;
                         justify-content: space-between;
                       `}
