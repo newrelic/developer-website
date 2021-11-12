5ba8db4ee0a9367bdc128f3499b7f34d42e3223e
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-11-12 14:43:55 -0800

    chore: swap hardcoded id to be grabbed from query param instead

--- src/pages/test-ssr.js
+++ src/pages/test-ssr.js
@@ -5,25 +5,10 @@ import propTypes from 'prop-types';
 const API_URL = 'https://staging-api.newrelic.com/graphql';
 const API_KEY = process.env.GATSBY_NEW_RELIC_API_KEY;
 
-const QUERY = `
-{
-  actor {
-    nr1Catalog {
-      quickstart(id: "d6b36ecf-bf99-475c-938f-370153db8e70") {
-        metadata {
-          displayName
-          summary
-        }
-      }
-    }
-  }
-}
-`;
-
 const prop = (k) => (x) => x[k];
 
 const TestSSR = ({ serverData }) => {
-  console.log('serverData', serverData);
+  console.log(JSON.stringify(serverData));
 
   if (!serverData) {
     return <h1>Loading...</h1>;
@@ -57,11 +42,32 @@ TestSSR.propTypes = {
   serverData: propTypes.object.isRequired,
 };
 
-export const getServerData = async () => {
+export const getServerData = async (context) => {
+  console.log(`context ---- ${JSON.stringify(context)}`);
+
+  const { query: queryString } = context;
+
+  const id = queryString.id || 'd6b36ecf-bf99-475c-938f-370153db8e70';
+
+  const temp = `
+{
+  actor {
+    nr1Catalog {
+      quickstart(id: "${id}") {
+        metadata {
+          displayName
+          summary
+        }
+      }
+    }
+  }
+}
+`;
+
   try {
     const resp = await fetch(API_URL, {
       method: 'POST',
-      body: JSON.stringify({ query: QUERY }),
+      body: JSON.stringify({ query: temp }),
       headers: {
         'Content-Type': 'application/json',
         'Api-Key': API_KEY,
