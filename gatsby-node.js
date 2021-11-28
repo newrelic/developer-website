1c97eb6ceb04889f18ebd200b49d319a96c63fba
Jerel Miller <jerelmiller@gmail.com>
2021-11-28 00:47:11 -0800

    chore: Add patch to schema to ensure the path field is always defined

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,27 @@ const kebabCase = (string) =>
     .replace(/\s+/g, '-')
     .toLowerCase();
 
+// This is a temporary customization to ensure that the path field is always
+// queryable from the createPages API. We rely on the path for deprecation
+// warnings to inform authors that path is no longer a supported markdown
+// property. Because of Gatsby's type inference from frontmatter properties, the
+// query in createPages fails without this patch if no file contains a 'path'
+// frontmatter property.
+//
+// This patch can be safely removed when removing the deprecation warning in
+// createPages.
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+
+  const typeDefs = `
+    type MdxFrontmatter {
+      path: String
+    }
+  `;
+
+  createTypes(typeDefs);
+};
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage, createRedirect } = actions;
 
