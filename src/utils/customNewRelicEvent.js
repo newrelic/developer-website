ec6994a3a708bf0bad8968adac1579e1bcef8035
Zack Stickles <zstickles@newrelic.com>
2021-12-15 15:18:05 -0800

    chore: added event tracking utility for custom NR Events
    NOTE: This is for non-browser "server" events. We don't have the node
    agent installed in the Gatsby function "worker" so we need to rely on
    New Relic custom events API.
    
    This will require adding our account ID to the environment, in addition
    to our API key (which is already in place).

---
+++ src/utils/customNewRelicEvent.js
@@ -0,0 +1,51 @@
+const fetch = require('node-fetch');
+
+const NEW_RELIC_ACCOUNT_ID = process.env.NEW_RELIC_ACCOUNT_ID;
+const NEW_RELIC_API_KEY = process.env.NEW_RELIC_API_KEY;
+
+/**
+ * Helper function to make an API request to the Events API.
+ *
+ * @param {string} key New Relic API key
+ * @param {string} accountId The New Relic account to send the request to
+ * @param {Object} data The data to be sent
+ * @returns {Promise<boolean>} Whether or not the request was (eventually) successful
+ */
+const apiRequest = async (key, accountId, data) => {
+  const url = `https://insights-collector.newrelic.com/v1/accounts/${accountId}/events`;
+
+  try {
+    await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+        'Api-Key': key,
+      },
+    });
+
+    return true;
+  } catch (e) {
+    /* eslint-disable-next-line no-console */
+    console.error('Unable to track custom event:', data, e);
+
+    return false;
+  }
+};
+
+/**
+ * Tracks an event in New Relic using the custom Events API.
+ *
+ * @param {Object} options
+ * @param {string} options.eventType The name of the event type to track
+ * @param {Object} [options.metadata] (Optional) metadata to attach to the event
+ * @returns {Promise<boolean>} Whether or not the request was (eventually) successful
+ */
+const track = async ({ eventType, metadata = {} }) =>
+  apiRequest(NEW_RELIC_API_KEY, NEW_RELIC_ACCOUNT_ID, {
+    eventType,
+    account: NEW_RELIC_ACCOUNT_ID,
+    ...metadata,
+  });
+
+export default track;
