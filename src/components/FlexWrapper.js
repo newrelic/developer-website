e2d712da8fd184fb174350181e12b63c06492e4e
kisielewskik <k.kisielewski.k@gmail.com>
2022-02-03 17:31:02 +0100

    feat: components for requirements and objectives

---
+++ src/components/FlexWrapper.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import PropTypes from 'prop-types';
+
+const FlexWrapper = ({ children }) => (
+  <div
+    css={css`
+      display: flex;
+      padding-bottom: 25px;
+    `}
+  >
+    {children}
+  </div>
+);
+
+FlexWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+export default FlexWrapper;
