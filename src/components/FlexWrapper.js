7a4d244069e478a31e2d16ed1fed5665ba34040f
kisielewskik <k.kisielewski.k@gmail.com>
2022-02-21 09:32:58 +0100

    fix: conflicts check

--- src/components/FlexWrapper.js
+++ src/components/FlexWrapper.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { css } from '@emotion/react';
 import PropTypes from 'prop-types';
 
-const FlexWrapper = ({ children }) => (
-  <div
-    css={css`
-      display: flex;
-      padding-bottom: 25px;
-    `}
-  >
-    {children}
-  </div>
-);
+const FlexWrapper = ({ children }) => {
+  console.log(children);
+
+  return (
+    <div
+      css={css`
+        display: flex;
+        padding-bottom: 25px;
+      `}
+    >
+      {children}
+    </div>
+  );
+};
 
 FlexWrapper.propTypes = {
   children: PropTypes.node.isRequired,
