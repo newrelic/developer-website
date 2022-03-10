ca38d4c642e56e6c51e9b29f3ff3a26f6500ead5
Clark McAdoo <jmcadoo@newrelic.com>
2022-03-10 16:35:06 -0600

    fix test - adding back dependencies and plugins to config to see if site will build

--- gatsby-config.js
+++ gatsby-config.js
@@ -167,6 +167,78 @@ module.exports = {
         path: `${__dirname}/src/markdown-pages`,
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'quickstarts',
+        path: `${__dirname}/src/data/quickstarts.json`,
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-json',
+      options: {
+        // If we need to source json files other than the i18n/nav, we should
+        // consider making this dynamic. See the docs for ways to do this.
+        //
+        // https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-transformer-json
+        typeName: 'Quickstarts',
+      },
+    },
+    'gatsby-remark-images',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxHeight: 400,
+              maxWidth: 1200,
+              fit: 'inside',
+              linkImagesToOriginal: false,
+            },
+          },
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              icon:
+                '<svg xmlns="http://www.w3.org/2000/svg" focusable="false" width="1rem" height="1rem" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 7h3a5 5 0 0 1 5 5 5 5 0 0 1-5 5h-3m-6 0H6a5 5 0 0 1-5-5 5 5 0 0 1 5-5h3"></path><line x1="8" y1="12" x2="16" y2="12"></line></svg>',
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-source-newrelic-sdk',
+      options: {
+        release: 'release-3439',
+      },
+    },
+    'gatsby-plugin-embed-pages',
+    {
+      resolve: 'gatsby-plugin-gdpr-tracking',
+      options: {
+        debug: false,
+        googleAnalytics: {
+          trackingId: 'UA-3047412-33',
+          autoStart: false,
+          anonymize: true,
+          controlCookieName: 'newrelic-gdpr-consent',
+        },
+        environments: ['production', 'development'],
+      },
+    },
+    'gatsby-plugin-use-query-params',
+    {
+      resolve: 'gatsby-plugin-gatsby-cloud',
+      options: {
+        allPageHeaders: [
+          'Referrer-Policy: no-referrer-when-downgrade',
+          'Content-Security-Policy: frame-ancestors *.newrelic.com *.skilljar.com *.sj-cdn.net',
+        ],
+      },
+    },
     // https://www.gatsbyjs.com/plugins/gatsby-plugin-typegen/
     {
       resolve: 'gatsby-plugin-typegen',
