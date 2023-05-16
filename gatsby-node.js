f05e14b41bd31aa28e7654c46eb36dd357effadd
mehreentahir16 <mehreen.dev@hotmail.com>
2023-05-16 15:45:24 +0100

    attempt at fixing redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -140,7 +140,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   });
 
   createRedirect({
-    fromPath: `/collect-data/browser/*`,
+    fromPath: `/collect-data/browser`,
     toPath: `https://docs.newrelic.com/docs/browser/new-relic-browser/lab/over-view`,
     isPermanent: true,
   });
