1a6c3369c6029851310d953f9c821654c60072b8
mehreentahir16 <mehreen.dev@hotmail.com>
2023-05-16 15:59:12 +0100

    fixed redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -140,13 +140,13 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   });
 
   createRedirect({
-    fromPath: `/collect-data/collect-data/browser/`,
+    fromPath: `/collect-data/browser/`,
     toPath: `https://docs.newrelic.com/docs/browser/new-relic-browser/lab/over-view`,
     isPermanent: true,
   });
 
   createRedirect({
-    fromPath: `/collect-data/collect-data/browser/*`,
+    fromPath: `/collect-data/browser/*`,
     toPath: `https://docs.newrelic.com/docs/browser/new-relic-browser/lab/over-view`,
     isPermanent: true,
   });
